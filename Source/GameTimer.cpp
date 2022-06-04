d2b33885ee70b286f9ccffbd82f07db2682bb592
DESKTOP-N6FOOG4\choij <choijione02@gmail.com>
2022-06-04 15:22:37 +0900

    GameTimer class 선언 및 구현

---
+++ Source/GameTimer.cpp
@@ -0,0 +1,73 @@
+#include "GameTimer.h"
+
+
+GameTimer::~GameTimer()
+{
+}
+
+float GameTimer::DeltaTime() const
+{
+	if (bIsStop)
+	{
+		return 0.0f;
+	}
+	else
+	{
+		return static_cast<float>(CurrentTime - PreviousTime) / 1000.0f;
+	}
+}
+
+float GameTimer::TotalTime() const
+{
+	if (bIsStop)
+	{
+		return static_cast<float>(StopTime - PausedTime - BaseTime) / 1000.0f;
+	}
+	else
+	{
+		return static_cast<float>(CurrentTime - PausedTime - BaseTime) / 1000.0f;
+	}
+}
+
+void GameTimer::Reset()
+{
+	uint64_t TickTime = SDL_GetTicks64();
+
+	BaseTime = TickTime;
+	PausedTime = 0ULL;
+	StopTime = 0ULL;
+	PreviousTime = TickTime;
+	CurrentTime = TickTime;
+}
+
+void GameTimer::Start()
+{
+	if (bIsStop)
+	{
+		uint64_t TickTime = SDL_GetTicks64();
+
+		PausedTime += (TickTime - StopTime);
+		PreviousTime = TickTime;
+		StopTime = 0ULL;
+
+		bIsStop = false;
+	}
+}
+
+void GameTimer::Stop()
+{
+	if (!bIsStop)
+	{
+		uint64_t TickTime = SDL_GetTicks64();
+
+		StopTime = TickTime;
+
+		bIsStop = true;
+	}
+}
+
+void GameTimer::Tick()
+{
+	PreviousTime = CurrentTime;
+	CurrentTime = SDL_GetTicks64();
+}
