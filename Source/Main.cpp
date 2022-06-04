a7fbbe5ecafa2592848bda4ed6289ae270c5e45c
DESKTOP-N6FOOG4\choij <choijione02@gmail.com>
2022-06-04 14:46:14 +0900

    Logger class 구현 테스트

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -1,6 +1,10 @@
-#include <iostream>
+#include "Logger.h"
 
 int main(int argc, char* argv[])
 {
+	Logger::Message("일반 메시지 출력");
+	Logger::Warning("경고 메시지 출력");
+	Logger::Error("에러 메시지 출력");
+
 	return 0;
 }
