b96b6d56910bc887e419f689c81281c52dcd6d4a
DESKTOP-4QAGCGF\choij <choijione02@gmail.com>
2022-06-09 07:14:27 +0900

    Grid와 텍스트 그리기 테스트 코드 삭제

--- Source/Game.cpp
+++ Source/Game.cpp
@@ -74,19 +74,9 @@ void Game::Run()
 		// 프레임 렌더링을 시작합니다.
 		GetGameRenderer().BeginFrame(ColorUtil::Black);
 
-		// 2D 평면 Grid를 그립니다.
-		for (int32_t x = 0; x <= 1000; x += 10)
-		{
-			GetGameRenderer().DrawLine2D(Vec2i(x, 0), Vec2i(x, 800), ColorUtil::White);
-		}
 
-		for (int32_t y = 0; y <= 800; y += 10)
-		{
-			GetGameRenderer().DrawLine2D(Vec2i(0, y), Vec2i(1000, y), ColorUtil::White);
-		}
-
-		GetGameRenderer().DrawText2D(Vec2i(100, 100), Font, L"English", ColorUtil::Green);
-		GetGameRenderer().DrawText2D(Vec2i(0, 35), Font, StringUtil::StringFormat(L"FPS : %.f", 1.0f / GlobalTimer.DeltaTime()), ColorUtil::Green);
+		// 화면에 FPS를 표시합니다.
+		GetGameRenderer().DrawText2D(Font, Vec2i(0, 35), StringUtil::StringFormat(L"FPS : %.f", 1.0f / GlobalTimer.DeltaTime()), ColorUtil::White);
 
 
 		// 프레임 렌더링을 종료하고, 벡 버퍼를 화면에 표시합니다.
