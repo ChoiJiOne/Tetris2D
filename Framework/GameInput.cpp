1b8453de68a350d8ddf4282537f891b7edf46b6e
DESKTOP-4QAGCGF\choij <choijione02@gmail.com>
2022-06-09 09:42:03 +0900

    GameInput 기능 추가
    - 키보드 입력 기능 추가
    - 마우스 입력 기능 추가
    - Window Event를 처리하는 콜백 함수 추가
    - 복사 생성자와 대입 연산자를 사용할 수 없도록 메서드 명시적으로 삭제

--- Framework/GameInput.cpp
+++ Framework/GameInput.cpp
@@ -1,44 +1,26 @@
+#include "StringUtil.h"
+#include "Logger.h"
 #include "GameInput.h"
 
 GameInput::~GameInput()
 {
+    bIsInitialize = false;
 }
 
-GameInput::GameInput(GameInput&& InInstance) noexcept
-    : ExitEventCallback(InInstance.ExitEventCallback)
-    , ResizeEventCallback(InInstance.ResizeEventCallback)
+void GameInput::Init()
 {
+    if (!bIsInitialize)
+    {
+        PrevKeyboardState.resize(SDL_Scancode::SDL_NUM_SCANCODES);
+        CurrKeyboardState.resize(SDL_Scancode::SDL_NUM_SCANCODES);
+
+        bIsInitialize = true;
+    }
 }
 
-GameInput& GameInput::operator=(GameInput&& InInstance) noexcept
+void GameInput::SetQuitEventCallback(const std::function<void()>& InQuitEventCallback)
 {
-    if (this == &InInstance) return *this;
-
-    ExitEventCallback = InInstance.ExitEventCallback;
-    ResizeEventCallback = InInstance.ResizeEventCallback;
-
-    return *this;
-}
-
-GameInput::GameInput(const GameInput& InInstance) noexcept
-    : ExitEventCallback(InInstance.ExitEventCallback)
-    , ResizeEventCallback(InInstance.ResizeEventCallback)
-{
-}
-
-GameInput& GameInput::operator=(const GameInput& InInstance) noexcept
-{
-    if (this == &InInstance) return *this;
-
-    ExitEventCallback = InInstance.ExitEventCallback;
-    ResizeEventCallback = InInstance.ResizeEventCallback;
-
-    return *this;
-}
-
-void GameInput::SetExitEventCallback(const std::function<void()>& InExitEventCallback)
-{
-    ExitEventCallback = InExitEventCallback;
+    QuitEventCallback = InQuitEventCallback;
 }
 
 void GameInput::SetResizeEventCallback(const std::function<void(int32_t, int32_t)>& InResizeEventCallback)
@@ -46,22 +28,163 @@ void GameInput::SetResizeEventCallback(const std::function<void(int32_t, int32_t
     ResizeEventCallback = InResizeEventCallback;
 }
 
+void GameInput::SetMinimizeEventCallback(const std::function<void()>& InMinimizeEventCallback)
+{
+    MinimizeEventCallback = InMinimizeEventCallback;
+}
+
+void GameInput::SetMaximizeEventCallback(const std::function<void()>& InMaximizeEventCallback)
+{
+    MaximizeEventCallback = InMaximizeEventCallback;
+}
+
+void GameInput::SetActiveWindowCallback(const std::function<void()>& InActiveWindowCallback)
+{
+    ActiveWindowCallback = InActiveWindowCallback;
+}
+
+void GameInput::SetInactiveWindowCallback(const std::function<void()>& InInactiveWindowCallback)
+{
+    InactiveWindowCallback = InInactiveWindowCallback;
+}
+
+void GameInput::SetExposeWindowCallback(const std::function<void()>& InExposeWindowCallback)
+{
+    ExposeWindowCallback = InExposeWindowCallback;
+}
+
 void GameInput::Tick() noexcept
 {
+    bool bIsUpdate = false;
     SDL_Event EventMessage = {};
 
     while (SDL_PollEvent(&EventMessage))
     {
+        bIsUpdate = true;
+
         if (EventMessage.type == SDL_QUIT)
         {
-            ExitEventCallback();
+            QuitEventCallback();
         }
 
-        if (EventMessage.type == SDL_WINDOWEVENT && EventMessage.window.event == SDL_WINDOWEVENT_SIZE_CHANGED)
+        if (EventMessage.type == SDL_WINDOWEVENT)
         {
-            int32_t Width = EventMessage.window.data1;
-            int32_t Height = EventMessage.window.data1;
-            ResizeEventCallback(Width, Height);
+            ProcessWindowEvent(EventMessage.window);
         }
     }
+
+    UpdateKeyboardState();
+    UpdateMouseState();
+}
+
+void GameInput::ProcessWindowEvent(const SDL_WindowEvent& InWindowEvent)
+{
+    SDL_Event Event;
+    int32_t ChangeWindowWidth = 0;
+    int32_t ChangeWindowHeight = 0;
+
+    switch (InWindowEvent.event)
+    {
+    case SDL_WINDOWEVENT_SIZE_CHANGED:
+        bIsMinimized = false;
+        bIsMaximized = false;
+
+        ChangeWindowWidth  = InWindowEvent.data1;
+        ChangeWindowHeight = InWindowEvent.data2;
+
+        ResizeEventCallback(ChangeWindowWidth, ChangeWindowHeight);
+        break;
+
+
+    case SDL_WINDOWEVENT_EXPOSED:
+        ExposeWindowCallback();
+        break;
+
+
+    case SDL_WINDOWEVENT_ENTER:
+        bIsMouseFocus = true;
+        break;
+
+
+    case SDL_WINDOWEVENT_LEAVE:
+        bIsMouseFocus = false;
+        break;
+
+
+    case SDL_WINDOWEVENT_FOCUS_GAINED:
+        bIsActiveWindow = true;
+        ActiveWindowCallback();
+        break;
+
+
+    case SDL_WINDOWEVENT_FOCUS_LOST:
+        bIsActiveWindow = false;
+        InactiveWindowCallback();
+        break;
+
+
+    case SDL_WINDOWEVENT_MINIMIZED:
+        bIsMinimized = true;
+        bIsMaximized = false;
+        MinimizeEventCallback();
+        break;
+
+
+    case SDL_WINDOWEVENT_MAXIMIZED:
+        bIsMinimized = false;
+        bIsMaximized = true;
+        MaximizeEventCallback();
+        break;
+
+
+    case SDL_WINDOWEVENT_RESTORED:
+        ExposeWindowCallback();
+        break;
+
+
+    case SDL_WINDOWEVENT_CLOSE:
+        Event.type = SDL_QUIT;
+        SDL_PushEvent(&Event);
+        break;
+
+
+    default:
+        break;
+    }
+}
+
+void GameInput::UpdateKeyboardState()
+{
+    const uint8_t* KeyState = SDL_GetKeyboardState(NULL);
+
+    std::memcpy(
+        reinterpret_cast<void*>(&PrevKeyboardState[0]),
+        reinterpret_cast<const void*>(&CurrKeyboardState[0]),
+        static_cast<size_t>(SDL_Scancode::SDL_NUM_SCANCODES)
+    );
+
+    std::memcpy(
+        reinterpret_cast<void*>(&CurrKeyboardState[0]),
+        reinterpret_cast<const void*>(&KeyState[0]),
+        static_cast<size_t>(SDL_Scancode::SDL_NUM_SCANCODES)
+    );
+}
+
+void GameInput::UpdateMouseState()
+{
+    PrevMousePositionX = CurrMousePositionX;
+    PrevMousePositionY = CurrMousePositionY;
+    PrevMouseState = CurrMouseState;
+
+    CurrMouseState = SDL_GetMouseState(&CurrMousePositionX, &CurrMousePositionY);
+}
+
+bool GameInput::IsPrevKeyPress(uint8_t InKeyCode) noexcept
+{
+    return PrevKeyboardState[InKeyCode] == 0 ? false : true;
+}
+
+bool GameInput::IsCurrKeyPress(uint8_t InKeyCode) noexcept
+{
+    return CurrKeyboardState[InKeyCode] == 0 ? false : true;
 }
--- Framework/GameInput.h
+++ Framework/GameInput.h
@@ -16,40 +16,179 @@ public:
 	virtual ~GameInput();
 
 
-	// 복사 생성자 및 대입 연산자입니다.
-	GameInput(GameInput&& InInstance) noexcept;
-	GameInput& operator=(GameInput&& InInstance) noexcept;
+	// 복사 생성자를 사용할 수 없도록 명시적으로 삭제합니다.
+	GameInput(GameInput&& InInstance) = delete;
+	GameInput(const GameInput& InInstance) = delete;
 
 
-	// 복사 생성자 및 대입 연산자입니다.
-	GameInput(const GameInput& InInstance) noexcept;
-	GameInput& operator=(const GameInput& InInstance) noexcept;
+	// 대입 연산자를 사용할 수 없도록 명시적으로 삭제합니다.
+	GameInput& operator=(GameInput&& InInstance) = delete;
+	GameInput& operator=(const GameInput& InInstance) = delete;
 
 
-	// 종료 이벤트 콜백 함수를 설정합니다.
+	// GameInput을 초기화합니다.
+	void Init();
+
+
+	// 종료 이벤트의 콜백 함수를 설정합니다.
 	//
-	// @param InExitEventCallback - 종료 이벤트 발생 시 호출할 이벤트입니다.
-	void SetExitEventCallback(const std::function<void()>& InExitEventCallback);
+	// @param InQuitEventCallback - 종료 이벤트 발생 시 호출할 함수입니다.
+	void SetQuitEventCallback(const std::function<void()>& InQuitEventCallback);
 
 
-	// 윈도우 크기 변경 콜백 함수를 설정합니다.
+	// 윈도우 크기 변경 이벤트의 콜백 함수를 설정합니다.
 	// 
-	// @param InResizeEventCallback - 윈도우 크기 변경 이벤트 발생 시 호출할 이벤트입니다.
+	// @param InResizeEventCallback - 윈도우 크기 변경 이벤트 발생 시 호출할 함수입니다.
 	void SetResizeEventCallback(const std::function<void(int32_t, int32_t)>& InResizeEventCallback);
 
 
-	// 키보드의 상태를 업데이트합니다.
+	// 윈도우 최소화 이벤트의 콜백 함수를 설정합니다.
+	//
+	// @param InMinimizeEventCallback - 윈도우 최소화 이벤트 발생 시 호출할 함수입니다.
+	void SetMinimizeEventCallback(const std::function<void()>& InMinimizeEventCallback);
+
+
+	// 윈도우 최대화 이벤트의 콜백 함수를 설정합니다.
+	//
+	// @param InMaximizeEventCallback - 윈도우 최대화 이벤트 발생 시 호출할 함수입니다.
+	void SetMaximizeEventCallback(const std::function<void()>& InMaximizeEventCallback);
+
+
+	// 윈도우 활성화 이벤트의 콜백 함수를 설정합니다.
+	//
+	// @param InActiveWindowCallback - 윈도우 활성화 이벤트 발생 시 호출할 함수입니다.
+	void SetActiveWindowCallback(const std::function<void()>& InActiveWindowCallback);
+
+
+	// 윈도우 비활성화 이벤트의 콜백 함수를 설정합니다.
+	//
+	// @param InInactiveWindowCallback - 윈도우 비활성화 이벤트 발생 시 호출할 함수입니다.
+	void SetInactiveWindowCallback(const std::function<void()>& InInactiveWindowCallback);
+
+
+	// 윈도우 출력 이벤트의 콜백 함수를 설정합니다.
+	//
+	// @param InExposeWindowCallback - 윈도우 출력 이벤트 발생 시 호출할 함수입니다.
+	void SetExposeWindowCallback(const std::function<void()>& InExposeWindowCallback);
+
+
+	// 입력 상태를 업데이트합니다.
 	// 이 메서드는 매 프레임 호출되어야 합니다.
-	// 
-	// @return - SDL 종료 이벤트가 감지되면 false 반환, 그렇지 않으면 true 반환합니다.
 	void Tick() noexcept;
 
 
+	// Tick 호출 이전에 특정 키가 눌렸는지의 여부를 반환합니다.
+	//
+	// @param InKeyCode - 키코드 값입니다.
+	// @return - 키가 눌렸다면, true, 눌리지 않았다면 false를 반환합니다.
+	bool IsPrevKeyPress(uint8_t InKeyCode) noexcept;
+
+
+	// Tick 호출 이후에 특정 키가 눌렸는지의 여부를 반환합니다.
+	//
+	// @param InKeyCode - 키코드 값입니다.
+	// @return - 키가 눌렸다면, true, 눌리지 않았다면 false를 반환합니다.
+	bool IsCurrKeyPress(uint8_t InKeyCode) noexcept;
+
+
+	// 마우스가 윈도우 창 내부에 있는지 확인합니다.
+	//
+	// @return 마우스가 윈도우 창 내부에 있는지의 여부를 반환합니다.
+	bool IsMouseFocus() noexcept { return bIsMouseFocus; }
+
+
 private:
-	// 종료 이벤트 콜벡 함수입니다.
-	std::function<void()> ExitEventCallback = nullptr;
+	// SDL2의 윈도우 이벤트를 처리합니다.
+	// 
+	// @param InEventCode - SDL2 윈도우 이벤트입니다.
+	void ProcessWindowEvent(const SDL_WindowEvent& InWindowEvent);
 
 
-	// 윈도우 크기 변경 콜백 함수입니다.
+	// SDL2 키보드의 상태를 업데이트합니다.
+	void UpdateKeyboardState();
+
+
+	// SDL2 마우스 상태를 업데이트합니다.
+	void UpdateMouseState();
+
+
+private:
+	// GameInput 초기화 상태를 확인합니다.
+	bool bIsInitialize = false;
+
+
+	// 마우스가 윈도우 창 내부에 있는지 확인합니다.
+	bool bIsMouseFocus = false;
+
+
+	// 윈도우 창이 활성화되어 있는지 확인합니다.
+	bool bIsActiveWindow = false;
+
+
+	// 윈도우 창의 최대화 여부를 확인합니다.
+	bool bIsMaximized = false;
+
+
+	// 윈도우 창의 최소화 여부를 확인합니다.
+	bool bIsMinimized = false;
+
+
+	// Tick 호출 이전의 윈도우 마우스 X 좌표입니다.
+	int32_t PrevMousePositionX = 0;
+
+
+	// Tick 호출 이전의 윈도우 마우스 Y 좌표입니다.
+	int32_t PrevMousePositionY = 0;
+
+	
+	// Tick 호출 이전의 윈도우 마우스 상태입니다.
+	uint32_t PrevMouseState = 0;
+
+
+	// Tick 호출 이후의 윈도우 마우스 X 좌표입니다.
+	int32_t CurrMousePositionX = 0;
+
+
+	// Tick 호출 이후의 윈도우 마우스 Y 좌표입니다.
+	int32_t CurrMousePositionY = 0;
+
+
+	// Tick 호출 이후의 윈도우 마우스 상태입니다.
+	uint32_t CurrMouseState = 0;
+
+
+	// 종료 이벤트의 콜벡 함수입니다.
+	std::function<void()> QuitEventCallback = nullptr;
+
+
+	// 윈도우 크기 변경 이벤트의 콜백 함수입니다.
 	std::function<void(int32_t, int32_t)> ResizeEventCallback = nullptr;
+
+
+	// 윈도우 최소화 이벤트의 콜백 함수입니다.
+	std::function<void()> MinimizeEventCallback = nullptr;
+	
+	
+	// 윈도우 최대화 이벤트의 콜백 함수입니다.
+	std::function<void()> MaximizeEventCallback = nullptr;
+
+
+	// 윈도우 활성화 이벤트의 콜백 함수입니다.
+	std::function<void()> ActiveWindowCallback = nullptr;
+
+
+	// 윈도우 비활성화 이벤트의 콜백 함수입니다.
+	std::function<void()> InactiveWindowCallback = nullptr;
+
+
+	// 윈도우 출력 이벤트의 콜백 함수입니다.
+	std::function<void()> ExposeWindowCallback = nullptr;
+
+
+	// Tick 호출 이전의 키보드 상태를 나타내는 벡터입니다.
+	std::vector<uint8_t> PrevKeyboardState;
+
+
+	// Tick 호출 이후의 키보드 상태를 나타내는 벡터입니다.
+	std::vector<uint8_t> CurrKeyboardState;
 };
