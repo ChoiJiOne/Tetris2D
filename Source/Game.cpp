511abe92a59113c2d373fe93200db74b8c015649
DESKTOP-4QAGCGF\choij <choijione02@gmail.com>
2022-06-09 15:18:12 +0900

    리소스 변경 후 텍스처 렌더링 테스트

--- Source/Game.cpp
+++ Source/Game.cpp
@@ -43,15 +43,27 @@ void Game::Setup()
 
 
 	// 게임 폰트를 생성합니다.
-	std::string FontPath = GetResourceDirectory() + "/font/Nanum.ttf";
+	std::string FontPath = GetResourceDirectory() + "font/Nanum.ttf";
 	Font.CreateGameFont(GetGameRenderer().GetRenderer(), FontPath, 32.0f);
+
+
+	// 테트리스 블럭 텍스처를 생성합니다.
+
 }
 
 void Game::Run()
 {
 	// 게임 타이머를 초기화합니다.
 	Timer.Reset();
-	
+
+
+	GameTexture2D Gray;
+	Gray.CreateTextureFromFile(GetGameRenderer().GetRenderer(), GetResourceDirectory() + "texture/GrayBlock.png");
+
+	int32_t Width = static_cast<int32_t>(static_cast<float>(Gray.GetWidth()) * 0.25f);
+	int32_t Height = static_cast<int32_t>(static_cast<float>(Gray.GetHeight()) * 0.25f);
+	Vec2i Position(200, 100);
+
 
 	// 루프를 수행합니다.
 	while (!bIsDone)
@@ -66,6 +78,21 @@ void Game::Run()
 
 		// 프레임 렌더링을 시작합니다.
 		GetGameRenderer().BeginFrame(ColorUtil::Black);
+
+		if (!bIsPaused)
+		{
+			GetGameRenderer().DrawText2D(Font, Vec2i(0, 35), StringUtil::StringFormat(L"FPS : %.f", 1.0f / Timer.DeltaTime()), ColorUtil::White);
+		}
+
+
+		for (int32_t x = 0; x < 12; ++x)
+		{
+			for (int32_t y = 0; y < 20; ++y)
+			{
+				Vec2i MovePosition(x * Width, y * Height);
+				GetGameRenderer().DrawTexture2D(Gray, Position + MovePosition, 0.25f, 0.25f);
+			}
+		}
 
 
 		// 프레임 렌더링을 종료하고, 벡 버퍼를 화면에 표시합니다.
