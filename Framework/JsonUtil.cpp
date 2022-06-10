87bfab2f9dee311912d67b7669f373b0f8ca9824
DESKTOP-4QAGCGF\choij <choijione02@gmail.com>
2022-06-10 16:26:22 +0900

    JsonUtil class 선언 및 구현

---
+++ Framework/JsonUtil.cpp
@@ -0,0 +1,24 @@
+#include "Macro.h"
+#include "JsonUtil.h"
+
+nlohmann::json JsonUtil::LoadJsonFile(const std::string& InPath)
+{
+	std::ifstream Ifs(InPath);
+	CHECK_FAILED((Ifs.is_open()), "failed to load json file");
+
+	nlohmann::json OutputJson;
+	Ifs >> OutputJson;
+
+	return OutputJson;
+}
+
+nlohmann::json JsonUtil::LoadJsonFile(const std::wstring& InPath)
+{
+	std::ifstream Ifs(InPath);
+	CHECK_FAILED((Ifs.is_open()), "failed to load json file");
+
+	nlohmann::json OutputJson;
+	Ifs >> OutputJson;
+
+	return OutputJson;
+}
