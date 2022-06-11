364769bafafb6c343c4c09487f09d9df7cf729c8
DESKTOP-4QAGCGF\choij <choijione02@gmail.com>
2022-06-11 18:32:32 +0900

    Tetromino class 선언 및 구현

--- Source/Tetromino.cpp
+++ Source/Tetromino.cpp
@@ -1,10 +1,225 @@
+#include "Public/GameEngine.h"
+#include "Public/GameRenderer.h"
+#include "Public/Macro.h"
 #include "Tetromino.h"
 
-Tetromino::Tetromino(const EShape& InShape, const Vec2i& InPositionInBoard)
+Tetromino::Tetromino(const EShape& InShape, const EBlockColor& InBlockColor, const Vec2i& InPositionInBoard)
 	: PositionInBoard(InPositionInBoard)
+	, BlockColor(InBlockColor)
 {
+	GenerateBlockPositions(InShape);
+	TetrominoTexture.CreateTextureFromFile(GameEngine::GetResourceDirectory() + "texture/GrayBlock.png");
 }
 
 Tetromino::~Tetromino()
 {
+}
+
+Tetromino::Tetromino(const Tetromino& InInstance) noexcept
+	: BoundSize(InInstance.BoundSize)
+	, PositionInBoard(InInstance.PositionInBoard)
+	, BlockPositions(InInstance.BlockPositions)
+	, BlockColor(InInstance.BlockColor)
+{
+}
+
+Tetromino& Tetromino::operator=(const Tetromino& InInstance) noexcept
+{
+	if (this == &InInstance) return *this;
+
+	BoundSize = InInstance.BoundSize;
+	PositionInBoard = InInstance.PositionInBoard;
+	BlockPositions = InInstance.BlockPositions;
+	BlockColor = InInstance.BlockColor;
+
+	return *this;
+}
+
+Tetromino::Tetromino(Tetromino&& InInstance) noexcept
+	: BoundSize(InInstance.BoundSize)
+	, PositionInBoard(InInstance.PositionInBoard)
+	, BlockPositions(InInstance.BlockPositions)
+	, BlockColor(InInstance.BlockColor)
+{
+}
+
+Tetromino& Tetromino::operator=(Tetromino&& InInstance) noexcept
+{
+	if (this == &InInstance) return *this;
+
+	BoundSize = InInstance.BoundSize;
+	PositionInBoard = InInstance.PositionInBoard;
+	BlockPositions = InInstance.BlockPositions;
+	BlockColor = InInstance.BlockColor;
+
+	return *this;
+}
+
+std::shared_ptr<Tetromino> Tetromino::GenerateRandomTetromino(const Vec2i& InPositionInBoard)
+{
+	static EShape TetrominoShapes[] = {
+		EShape::I,
+		EShape::O,
+		EShape::T,
+		EShape::J,
+		EShape::L,
+		EShape::S,
+		EShape::Z
+	};
+
+	static EBlockColor TetrominoColors[] = {
+		EBlockColor::BLUE,
+		EBlockColor::CYAN,
+		EBlockColor::GRAY,
+		EBlockColor::GREEN,
+		EBlockColor::ORANGE,
+		EBlockColor::PINK,
+		EBlockColor::PURPLE,
+		EBlockColor::RED,
+		EBlockColor::YELLOW
+	};
+
+	int32_t TetrominoShape = MathUtil::GenerateRandomInt<int32_t>(0, std::size(TetrominoShapes) - 1);
+	int32_t TetrominoColor = MathUtil::GenerateRandomInt<int32_t>(0, std::size(TetrominoColors) - 1);
+
+	return std::make_shared<Tetromino>(
+		TetrominoShapes[TetrominoShape],
+		TetrominoColors[TetrominoColor],
+		InPositionInBoard
+	);
+}
+
+void Tetromino::Spin(ESpin InSpin)
+{
+	switch (InSpin)
+	{
+	case ESpin::CW:
+		BlockPositions[0] = Vec2i(BoundSize - 1 - BlockPositions[0].y, BlockPositions[0].x);
+		BlockPositions[1] = Vec2i(BoundSize - 1 - BlockPositions[1].y, BlockPositions[1].x);
+		BlockPositions[2] = Vec2i(BoundSize - 1 - BlockPositions[2].y, BlockPositions[2].x);
+		BlockPositions[3] = Vec2i(BoundSize - 1 - BlockPositions[3].y, BlockPositions[3].x);
+		break;
+
+	case ESpin::CCW:
+		BlockPositions[0] = Vec2i(BlockPositions[0].y, BoundSize - 1 - BlockPositions[0].x);
+		BlockPositions[1] = Vec2i(BlockPositions[1].y, BoundSize - 1 - BlockPositions[1].x);
+		BlockPositions[2] = Vec2i(BlockPositions[2].y, BoundSize - 1 - BlockPositions[2].x);
+		BlockPositions[3] = Vec2i(BlockPositions[3].y, BoundSize - 1 - BlockPositions[3].x);
+		break;
+
+	default:
+		Logger::Warning("not support spin type");
+	}
+}
+
+void Tetromino::Move(EMove InMove)
+{
+	switch (InMove)
+	{
+	case EMove::Left:
+		PositionInBoard.x -= 1;
+		break;
+
+	case EMove::Right:
+		PositionInBoard.x += 1;
+		break;
+
+	case EMove::Down:
+		PositionInBoard.y += 1;
+		break;
+
+	case EMove::Up:
+		PositionInBoard.y -= 1;
+		break;
+
+	default:
+		Logger::Warning("not support move type");
+	}
+}
+
+void Tetromino::DrawTetromino(const Vec2i& InPosition, float InScale)
+{
+	int32_t TextureWidth = 0, TextureHeight = 0;
+
+	TextureWidth  = static_cast<int32_t>(static_cast<float>(TetrominoTexture.GetWidth()) * InScale);
+	TextureHeight = static_cast<int32_t>(static_cast<float>(TetrominoTexture.GetHeight()) * InScale);
+
+	for (const auto& BlockPosition : BlockPositions)
+	{
+		Vec2i MovePosition(BlockPosition.x * TextureWidth, BlockPosition.y * TextureHeight);
+
+		GameEngine::GetGameRenderer().DrawTexture2D(
+			TetrominoTexture,
+			InPosition + MovePosition,
+			InScale,
+			InScale
+		);
+	}
+}
+
+void Tetromino::GenerateBlockPositions(const EShape& InShape)
+{
+	BlockPositions.resize(4);
+
+	switch (InShape)
+	{
+	case EShape::I:
+		BoundSize = 4;
+		BlockPositions[0] = Vec2i(0, 1);
+		BlockPositions[1] = Vec2i(1, 1);
+		BlockPositions[2] = Vec2i(2, 1);
+		BlockPositions[3] = Vec2i(3, 1);
+		break;
+
+	case EShape::O:
+		BoundSize = 4;
+		BlockPositions[0] = Vec2i(1, 1);
+		BlockPositions[1] = Vec2i(2, 1);
+		BlockPositions[2] = Vec2i(1, 2);
+		BlockPositions[3] = Vec2i(2, 2);
+		break;
+
+	case EShape::T:
+		BoundSize = 3;
+		BlockPositions[0] = Vec2i(0, 1);
+		BlockPositions[1] = Vec2i(1, 1);
+		BlockPositions[2] = Vec2i(2, 1);
+		BlockPositions[3] = Vec2i(1, 2);
+		break;
+
+	case EShape::J:
+		BoundSize = 3;
+		BlockPositions[0] = Vec2i(0, 1);
+		BlockPositions[1] = Vec2i(1, 1);
+		BlockPositions[2] = Vec2i(2, 1);
+		BlockPositions[3] = Vec2i(2, 2);
+		break;
+
+	case EShape::L:
+		BoundSize = 3;
+		BlockPositions[0] = Vec2i(0, 1);
+		BlockPositions[1] = Vec2i(1, 1);
+		BlockPositions[2] = Vec2i(2, 1);
+		BlockPositions[3] = Vec2i(0, 2);
+		break;
+
+	case EShape::S:
+		BoundSize = 3;
+		BlockPositions[0] = Vec2i(1, 0);
+		BlockPositions[1] = Vec2i(2, 0);
+		BlockPositions[2] = Vec2i(0, 1);
+		BlockPositions[3] = Vec2i(1, 1);
+		break;
+
+	case EShape::Z:
+		BoundSize = 3;
+		BlockPositions[0] = Vec2i(0, 0);
+		BlockPositions[1] = Vec2i(0, 1);
+		BlockPositions[2] = Vec2i(1, 1);
+		BlockPositions[3] = Vec2i(2, 1);
+		break;
+
+	default:
+		Logger::Warning("not support tetromino shape");
+	}
 }
--- Source/Tetromino.h
+++ Source/Tetromino.h
@@ -43,17 +43,77 @@ public:
 	// 생성자입니다.
 	// 
 	// @param InShape - 테트로미노의 모양입니다.
+	// @param InBlockColor - 테트로미노 블럭 색상입니다.
 	// @param InPositionInBoard - 테트로미노의 보드 상의 위치입니다.
-	Tetromino(const EShape& InShape, const Vec2i& InPositionInBoard);
+	Tetromino(const EShape& InShape, const EBlockColor& InBlockColor, const Vec2i& InPositionInBoard);
 
 
 	// 가상 소멸자입니다.
 	virtual ~Tetromino();
 
 
+	// 복사 생성자입니다.
+	Tetromino(const Tetromino& InInstance) noexcept;
+	Tetromino& operator=(const Tetromino& InInstance) noexcept;
+
+
+	// 대입 연산자입니다.
+	Tetromino(Tetromino&& InInstance) noexcept;
+	Tetromino& operator=(Tetromino&& InInstance) noexcept;
+
+
+	// 렌덤하게 테트로미노를 생성합니다.
+	static std::shared_ptr<Tetromino> GenerateRandomTetromino(const Vec2i& InPositionInBoard);
+
+
+	// 테트로미노를 회전합니다.
+	//
+	// @param InSpin - 테트로미노의 회전 방향입니다.
+	void Spin(ESpin InSpin);
+
+
+	// 테트로미노를 움직입니다.
+	//
+	// @param InMove - 테트로미노의 이동 방향입니다.
+	void Move(EMove InMove);
+
+
+	// 테트로미노의 정사각형 영역의 크기를 반환합니다.
+	//
+	// @return - 테트로미노의 정사각형 영역의 크기를 반환합니다.
+	int32_t GetBoundSize() const { return BoundSize; }
+
+
+	// 테트리스 보드 상의 위치를 반환합니다.
+	//
+	// @return - 테트리스 보드 상의 위치를 반환합니다.
+	Vec2i GetPositionInBoard()  noexcept { return PositionInBoard; }
+	
+	
+	// 테트로미노 블록의 위치들을 반환합니다. 
+	//
+	// @return - 테트로미노 블럭의 위치를 포함한 Vec2i 벡터를 반환합니다.
+	const std::vector<Vec2i>& GetBlockPositions() noexcept { return BlockPositions; }
+
+
+	// 테트로미노 블록의 색상을 반환합니다.
+	//
+	// @return - 테트로미노 블록의 색상을 반환합니다.
+	EBlockColor GetBlockColor() const { return BlockColor; }
+
+
+	// 테트로미노를 화면에 그립니다.
+	//
+	// @param InPosition - 윈도우 좌표계를 기준으로 테트로미노 정사각형 영역의 왼쪽 상단점입니다.
+	// @param InScale - 렌더링 할 텍스처의 Scale 값입니다.
+	void DrawTetromino(const Vec2i& InPosition, float InScale);
+
 
 private:
-	// void GenerateTetromino(const EShape& InShape);
+	// 테트로미노 블럭의 점을 생성합니다.
+	//
+	// @param InShape - 테트로미노의 타입입니다.
+	void GenerateBlockPositions(const EShape& InShape);
 
 
 private:
@@ -67,4 +127,12 @@ private:
 
 	// 테트로미노 블럭들의 상대 위치입니다.
 	std::vector<Vec2i> BlockPositions;
+
+
+	// 테트로미노 전체 블럭의 색상입니다.
+	EBlockColor BlockColor;
+
+
+	// 테트로미노의 텍스처입니다.
+	GameTexture2D TetrominoTexture;
 };
