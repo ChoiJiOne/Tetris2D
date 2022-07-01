9291b755bf91726e01c718b4d84c94a6949b99c5
DESKTOP-4QAGCGF\choij <choijione02@gmail.com>
2022-07-01 10:53:55 +0900

    Main.cpp 에서 GameString 테스트 진행

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -1,6 +1,9 @@
+#include "Game/GameString.h"
+
 #include "SDL2/SDL.h"
 
 int main(int argc, char* argv[])
 {
+	std::cout << Game::String::Format("Hello World Hash : %d\n", Game::String::GetHash("Hello World"));
 	return 0;
 }
