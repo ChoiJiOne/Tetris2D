16b353f0961017d11e284eae0415218a448e4958
DESKTOP-4QAGCGF\choij <choijione02@gmail.com>
2022-07-01 12:06:40 +0900

    SDLUtility class 구현 테스트
    - Main.cpp 에 메모리 누수를 감지하는 코드 추가
    - C++ 예외 처리 부분 추가

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -1,9 +1,38 @@
 #include "Game/GameString.h"
+#include "Game/GameSDLUtility.h"
 
 #include "SDL2/SDL.h"
 
 int main(int argc, char* argv[])
 {
-	std::cout << Game::String::Format("Hello World Hash : %d\n", Game::String::GetHash("Hello World"));
+#if defined(DEBUG) || defined(_DEBUG)
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+#endif
+
+	try
+	{
+		Game::SDLUtility::Init();
+
+		SDL_Window* Window = Game::SDLUtility::CreateSDLWindow(
+			"Tetris2D",
+			SDL_WINDOWPOS_CENTERED,
+			SDL_WINDOWPOS_CENTERED,
+			700,
+			800,
+			SDL_WINDOW_SHOWN | SDL_WINDOW_ALLOW_HIGHDPI
+		);
+		Sleep(3000);
+
+		Game::SDLUtility::SetWindowTitle(Window, "Change Title Name");
+		Sleep(3000);
+
+		Game::SDLUtility::DestroySDLWindow(Window);
+	}
+	catch (const std::exception& Exception)
+	{
+		std::cout << Exception.what() << std::endl;
+	}
+
+	Game::SDLUtility::Release();
 	return 0;
 }
