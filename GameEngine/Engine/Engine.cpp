50c396680997ca6bea79c28a5950926d2bdd15c1
DESKTOP-4QAGCGF\choij <choijione02@gmail.com>
2022-07-13 16:37:28 +0900

    Engine 내에 SDL 초기화 코드 추가

--- GameEngine/Engine/Engine.cpp
+++ GameEngine/Engine/Engine.cpp
@@ -1,9 +1,17 @@
+// @third party code - BEGIN SDL2
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_syswm.h>
+// @third party code - END
+
+#include "Macro.h"
 #include "Engine.h"
 
 Game::Engine::~Engine()
 {
+	SDL_Quit();
 }
 
 void Game::Engine::Initialize()
 {
+	CHECK((SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER | SDL_INIT_AUDIO | SDL_INIT_EVENTS) == 0), SDL_GetError());
 }
