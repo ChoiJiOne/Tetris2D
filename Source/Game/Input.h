e39ef2360562f149273121033dab4e7cb6badb03
DESKTOP-4QAGCGF\choij <choijione02@gmail.com>
2022-07-15 10:55:01 +0900

    주석문 JavaDoc에 맞게 수정

--- Source/Game/Input.h
+++ Source/Game/Input.h
@@ -5,15 +5,17 @@
 
 namespace Game
 {
-	// 버튼 입력 상태를 나타냅니다.
-	// --------------------------------------
-	// | 이전 프레임 | 현재 프레임 | 입력 상태 |
-	// --------------------------------------
-	// |     0      |     0      | None     |
-	// |     0      |     1      | Pressed  |
-	// |     1      |     0      | Released |
-	// |     1      |     1      | Held     |
-	// --------------------------------------
+	/**
+	 * 버튼 입력 상태를 나타냅니다.
+	 * --------------------------------------
+	 * | 이전 프레임 | 현재 프레임 | 입력 상태 |
+	 * --------------------------------------
+	 * |     0      |     0      | None     |
+	 * |     0      |     1      | Pressed  |
+	 * |     1      |     0      | Released |
+	 * |     1      |     1      | Held     |
+	 * --------------------------------------
+	 */
 	enum class EButtonState
 	{
 		None     = 0,
@@ -22,38 +24,50 @@ namespace Game
 		Held     = 3
 	};
 
-
-	// 키보드 상태를 나타내는 클래스입니다.
+	
+	/**
+	 * 키보드 상태를 나타내는 클래스입니다.
+	 */
 	class KeyboardState
 	{
 	public:
-		// 생성자입니다.
+		/**
+		 * 키보드 상태를 나타내는 클래스의 생성자입니다.
+		 */
 		KeyboardState();
 
 
-		// 가상 소멸자입니다.
+		/**
+		 * 키보드 상태를 나타내는 클래스의 가상 소멸자입니다.
+		 */
 		virtual ~KeyboardState();
 
 
-		// 키보드의 상태를 업데이트합니다.
+		/**
+		 * 키보드의 상태를 업데이트합니다.
+		 */
 		void Update();
 
 
-		// 키의 상태를 반환합니다.
-		//
-		// @param InKeyCode - 키 상태를 확인할 키 코드 값입니다.
-		//
-		// @return - 키의 버튼 상태를 반환합니다.
+		/**
+		 * 키의 상태를 반환합니다.
+		 * 
+		 * @param InKeyCode - 키 상태를 확인할 키 코드 값입니다.
+		 * 
+		 * @return 키의 버튼 상태를 반환합니다.
+		 */
 		EButtonState GetKeyState(uint8_t InKeyCode) const;
 
 
 	private:
-		// 특정 키가 눌렸는지 확인합니다.
-		//
-		// @param InKeyboardState - 검사를 수행할 키보드의 상태입니다.
-		// @param InKeyCode - 검사를 수행할 키입니다.
-		//
-		// @return 만약 키를 눌렀다면 true, 그렇지 않다면 false를 반환합니다.
+		/**
+		 * 특정 키가 눌렸는지 확인합니다.
+		 * 
+		 * @param InKeyboardState - 검사를 수행할 키보드의 상태입니다.
+		 * @param InKeyCode - 검사를 수행할 키입니다.
+		 * 
+		 * @return 만약 키를 눌렀다면 true, 그렇지 않다면 false를 반환합니다.
+		 */
 		bool IsPressKey(const std::vector<uint8_t>& InKeyboardState, uint8_t InKeyCode) const;
 
 
