3890ef3e3079612a29b8f3781bf78eb304078bc4
DESKTOP-4QAGCGF\choij <choijione02@gmail.com>
2022-07-16 22:53:06 +0900

    Player class 선언
    - TODO : Player class 구현

---
+++ Source/Player.cpp
@@ -0,0 +1,20 @@
+#include "Game/Graphics.h"
+#include "Game/Input.h"
+#include "Player.h"
+
+Player::~Player()
+{
+}
+
+void Player::Initialize()
+{
+
+}
+
+void Player::Update(const Game::Input& InInput)
+{
+}
+
+void Player::Draw(const Game::Graphics& InGraphics)
+{
+}
