aa6b4ac6107c6c94b4a69e34b1956c29d13eda43
DESKTOP-4QAGCGF\choij <choijione02@gmail.com>
2022-07-20 20:18:13 +0900

    기존의 코드 다시 원상복구
    - TODO : 타이머 코드 수정

---
+++ Source/Game/Timer.h
@@ -0,0 +1,104 @@
+#pragma once
+
+#include <cstdint>
+
+
+namespace Game
+{
+	/**
+	 * 게임 타이머 클래스입니다.
+	 */
+	class Timer
+	{
+	public:
+		/**
+		 * 타이머 클래스의 생성자입니다.
+		 *
+		 * @note 생성자에서 초기화를 수행하지 않으므로 반드시 Start 혹은 Reset 메서드를 호출해야 합니다.
+		 */
+		Timer() = default;
+
+
+		/**
+		 * 타이머 클래스의 가상 소멸자입니다.
+		 */
+		virtual ~Timer();
+
+
+		/**
+		 * 타이머의 델타 시간값을 반환합니다.
+		 *
+		 * @return 밀리초 단위의 델타 시간값을 반환합니다.
+		 */
+		float DeltaTime() const;
+
+
+		/**
+		 * 타이머가 중지된 시간을 제외한 전체 시간값을 반환합니다.
+		 *
+		 * @return 타이머가 시작된 이후의 중지된 시간을 제외한 전체 시간값을 밀리초 단위로 반환합니다.
+		 */
+		float TotalTime() const;
+
+
+		/**
+		 * 타이머의 모든 상태를 리셋합니다.
+		 */
+		void Reset();
+
+
+		/**
+		 * 타이머를 활성화시킵니다.
+		 */
+		void Start();
+
+
+		/**
+		 * 타이머를 중지시킵니다.
+		 */
+		void Stop();
+
+
+		/**
+		 * 타이머를 업데이트합니다.
+		 */
+		void Tick();
+
+
+	private:
+		/**
+		 * 타이머의 중지 여부를 확인합니다.
+		 */
+		bool bIsStop = false;
+
+
+		/**
+		 * 타이머가 시작된 시간을 나타냅니다.
+		 */
+		uint64_t BaseTime = 0ULL;
+
+
+		/**
+		 * 타이머가 중지된 동안의 누적 시간을 나타냅니다.
+		 */
+		uint64_t PausedTime = 0ULL;
+
+
+		/**
+		 * 타이머가 중지된 시간을 나타냅니다.
+		 */
+		uint64_t StopTime = 0ULL;
+
+
+		/**
+		 * 직전의 Tick 호출 시간을 나타냅니다.
+		 */
+		uint64_t PreviousTime = 0ULL;
+
+
+		/**
+		 * Tick 호출 시간을 나타냅니다.
+		 */
+		uint64_t CurrentTime = 0ULL;
+	};
+}
