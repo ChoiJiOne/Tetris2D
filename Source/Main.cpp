3cbab3ed6505c20c18fe6f458f4d34e7dfa5a593
DESKTOP-4QAGCGF\choij <choijione02@gmail.com>
2023-01-13 14:53:38 +0900

    윈도우 창 생성 및 아무 동작도 수행하지 않는 루프 추가

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -2,7 +2,72 @@
 
 #include <windows.h>
 
+LRESULT CALLBACK WindowMessageHandler(HWND WindowHandle, uint32_t Message, WPARAM WParam, LPARAM LParam)
+{
+	switch (Message)
+	{
+	case WM_CLOSE:
+		DestroyWindow(WindowHandle);
+		break;
+
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+
+	default:
+		return DefWindowProc(WindowHandle, Message, WParam, LParam);
+	}
+
+	return 0;
+}
+
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR CmdLine, int32_t CmdShow)
 {
+	WNDCLASSEX wcex;
+	wcex.cbSize = sizeof(WNDCLASSEX);
+	wcex.style = CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc = WindowMessageHandler;
+	wcex.cbClsExtra = 0;
+	wcex.cbWndExtra = 0;
+	wcex.hInstance = hInstance;
+	wcex.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	wcex.lpszMenuName = NULL;
+	wcex.lpszClassName = L"Tetris2D";
+	wcex.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+	RegisterClassEx(&wcex);
+
+	RECT rc = { 0, 0, 800, 600 };
+	AdjustWindowRect(&rc, WS_OVERLAPPEDWINDOW, FALSE);
+
+	HWND WindowHandle = CreateWindow(
+		L"Tetris2D", 
+		L"Tetris2D",
+		WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX,
+		CW_USEDEFAULT, CW_USEDEFAULT, 
+		rc.right - rc.left, rc.bottom - rc.top, 
+		nullptr, 
+		nullptr, 
+		hInstance,
+		nullptr
+	);
+
+	ShowWindow(WindowHandle, CmdShow);
+
+	MSG msg = { 0 };
+	while (WM_QUIT != msg.message)
+	{
+		if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+		else
+		{
+
+		}
+	}
+
 	return 0;
 }
