a7bde7894760d31558914058b527bd9fb3e722a7
DESKTOP-4QAGCGF\choij <choijione02@gmail.com>
2023-01-16 18:25:10 +0900

    Visual Studio 를 실행하지 않고도 빌드를 수행하는 스크립트 추가

---
+++ Build.bat
@@ -0,0 +1,11 @@
+@echo off
+
+echo start build...
+SET option=%1
+
+if not exist Tetris2D (
+    echo generate visual studio solution...
+    Bin\\premake5.exe vs2019
+)
+
+msbuild.exe Tetris2D\Tetris2D.sln -property:Configuration=%option% -target:Rebuild
