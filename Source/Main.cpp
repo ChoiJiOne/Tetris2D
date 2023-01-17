c7504aab8dce5fe4d744b06c430ad8741559e4ea
DESKTOP-N6FOOG4\choij <choijione02@gmail.com>
2023-01-17 14:53:39 +0900

    매크로 함수의 위치 변경 사항 적용

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -5,22 +5,7 @@
 #include <windows.h>
 #include <d3d11_1.h>
 
-
-/**
- * @brief COM 리소스를 할당 해제합니다.
- * 
- * @param X 할당 해제할 COM 리소스입니다.
- */
-#ifndef SAFE_RELEASE
-#define SAFE_RELEASE(X)\
-{\
-	if(X)\
-	{\
-		X->Release();\
-		X = nullptr;\
-	}\
-}
-#endif
+#include "Macro.h"
 
 
 /**
