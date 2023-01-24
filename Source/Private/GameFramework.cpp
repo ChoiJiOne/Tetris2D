fc5302f3969cfe2b04e150e681705a8429f3dd6d
DESKTOP-4QAGCGF\choij <choijione02@gmail.com>
2023-01-24 15:53:09 +0900

    하위 클래스의 윈도우 및 루프 중단 여부를 확인하는 변수를 상위 클래스로 이동

---
+++ Source/Private/GameFramework.cpp
@@ -0,0 +1,36 @@
+#include "GameFramework.h"
+#include "ContentManager.h"
+#include "Config.h"
+#include "Debug.h"
+#include "GraphicsManager.h"
+#include "Shader.h"
+#include "Window.h"
+
+GameFramework::GameFramework()
+{
+	SetUnhandledExceptionFilter(UnhandledExceptionHandler);
+
+	CreateWindowFromConfig();
+	GraphicsManager::Get().Init(Window_.get());
+}
+
+GameFramework::~GameFramework()
+{
+	GraphicsManager::Get().Cleanup();
+	Window_.reset();
+}
+
+void GameFramework::CreateWindowFromConfig()
+{
+	Config& WindowConfig = ContentManager::Get().LoadConfig("Window", "Window.config");
+
+	WindowConstructorParam Param;
+	Param.Title = Convert(WindowConfig.GetValue("title"));
+	Param.PositionX = std::stoi(WindowConfig.GetValue("x"));
+	Param.PositionY = std::stoi(WindowConfig.GetValue("y"));
+	Param.Width = std::stoi(WindowConfig.GetValue("width"));
+	Param.Height = std::stoi(WindowConfig.GetValue("height"));
+	Param.bIsFullScreen = std::stoi(WindowConfig.GetValue("fullscreen")) ? true : false;
+
+	Window_ = std::make_unique<Window>(Param);
+}
--- Source/Public/GameFramework.h
+++ Source/Public/GameFramework.h
@@ -2,9 +2,11 @@
 
 #include "Macro.h"
 
+class Window;
+
 
 /**
- * @brief 게임을 초기화 및 실행하는 프레임워크입니다.
+ * @brief 게임 프레임워크입니다.
  * 
  * @note 게임이 구현되는 클래스는 이 프레임워크를 반드시 상속받아야 합니다.
  */
@@ -14,18 +16,18 @@ public:
 	/**
 	 * @brief 게임 프레임워크의 생성자입니다.
 	 */
-	GameFramework() = default;
+	GameFramework();
 
 
 	/**
 	 * @brief 게임 프레임워크의 가상 소멸자입니다.
 	 */
-	virtual ~GameFramework() = default;
+	virtual ~GameFramework();
 
 
 	/**
 	 * @brief 게임 프레임워크를 초기화합니다.
-	 * 
+	 *
 	 * @note 가상 함수로, 반드시 하위 클래스에서 구현해야 합니다.
 	 */
 	virtual void Init() = 0;
@@ -37,4 +39,28 @@ public:
 	 * @note 가상 함수로, 반드시 하위 클래스에서 구현해야 합니다.
 	 */
 	virtual void Run() = 0;
+
+
+private:
+	/**
+	 * @brief 설정 파일로부터 윈도우 창을 생성합니다.
+	 * 
+	 * @throws 
+	 * 설정 파일 로딩 및 파싱에 실패하면 C++ 표준 예외를 던집니다.
+	 * 윈도우 창 생성에 실패하면 C++ 표준 예외를 던집니다.
+	 */
+	void CreateWindowFromConfig();
+
+
+protected:
+	/**
+	 * @brief 윈도우 창입니다.
+	 */
+	std::unique_ptr<Window> Window_ = nullptr;
+
+
+	/**
+	 * @brief 게임 루프를 수행할 지 확인합니다.
+	 */
+	bool bIsDone_ = false;
 };
