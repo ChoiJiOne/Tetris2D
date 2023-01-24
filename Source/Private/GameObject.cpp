2847b88d8b5a1d4d170a32e7acfb42a39095b894
DESKTOP-4QAGCGF\choij <choijione02@gmail.com>
2023-01-24 20:29:49 +0900

    GameObject를 2D 월드에 등록하는 기능 추가

--- Source/Private/GameObject.cpp
+++ Source/Private/GameObject.cpp
@@ -1,12 +1,15 @@
 #include "GameObject.h"
 #include "Component.h"
 #include "Camera2D.h"
+#include "WorldManager.h"
 
 GameObject::GameObject(const std::string& Signature)
 	: Signature_(Signature)
 {
+	WorldManager::Get().RegisterOjbect(this, Signature_);
 }
 
 GameObject::~GameObject()
 {
+	WorldManager::Get().UnregisterObject(Signature_);
 }
