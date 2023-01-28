ba783befc6ade2cec90d086dbf3b14a125b40e81
DESKTOP-N6FOOG4\choij <choijione02@gmail.com>
2023-01-28 15:16:42 +0900

    보드의 세로줄 처리 관련 기능 추가

--- Source/Private/Board.cpp
+++ Source/Private/Board.cpp
@@ -8,6 +8,8 @@
 #include "Utility.hpp"
 #include "WorldManager.h"
 
+float Board::MaxAccrueTickTime_ = 1.0f;
+
 Board::Board(const std::string& Signature, const Vec2i& TilePosition, const int32_t& Width, const int32_t& Height)
 	: GameObject(Signature)
 	, TilePosition_(TilePosition)
@@ -15,13 +17,47 @@ Board::Board(const std::string& Signature, const Vec2i& TilePosition, const int3
 	, Height_(Height)
 {
 	Tiles_ = CreateBoardTile(TilePosition_, Width_, Height_);
-
-	TileMap* Object = WorldManager::Get().GetGameObject<TileMap>("TileMap");
-	Object->AddTilesInMap(Tiles_);
+	DumpTileMapFromBoard();
 }
 
 void Board::Tick(float DeltaSeconds)
 {
+	if(State_ == EState::WAIT)
+	{
+		DumpBoardFromTileMap();
+		AccrueTickTime_ += DeltaSeconds;
+
+		if (AccrueTickTime_ > MaxAccrueTickTime_)
+		{
+			bool bHaveRemoveRowLine = false;
+			int32_t RemoveYPosition = 0;
+
+			for (int32_t BoardY = Height_ - 2; BoardY >= 1; --BoardY)
+			{
+				if (CanRemoveRowLine(BoardY))
+				{
+					bHaveRemoveRowLine = true;
+					RemoveYPosition = BoardY;
+					break;
+				}
+			}
+
+			if (bHaveRemoveRowLine)
+			{
+				ClearRowLine(RemoveYPosition);
+				RemoveLine_++;
+			}
+			else
+			{
+				ArrangeEmptyRowLine();
+				State_ = EState::ACTIVE;
+			}
+
+			AccrueTickTime_ = 0.0f;
+		}
+
+		DumpTileMapFromBoard();
+	}
 }
 
 std::vector<Tile> Board::CreateBoardTile(const Vec2i& TilePosition, const int32_t& Width, const int32_t& Height)
@@ -58,4 +94,93 @@ std::vector<Tile> Board::CreateBoardTile(const Vec2i& TilePosition, const int32_
 	}
 
 	return Tiles;
+}
+
+void Board::DumpBoardFromTileMap()
+{
+	TileMap* Object = WorldManager::Get().GetGameObject<TileMap>("TileMap");
+
+	Vec2i BoardPosition;
+	for (int32_t BoardX = 0; BoardX < Width_; ++BoardX)
+	{
+		for (int32_t BoardY = 0; BoardY < Height_; ++BoardY)
+		{
+			BoardPosition = Vec2i(BoardX, BoardY);
+			Vec2i Position = TilePosition_ + BoardPosition;
+
+			Tiles_[GetOffset(BoardPosition.x, BoardPosition.y, Width_, Height_)] = Object->GetTileInMap(Position);
+		}
+	}
+}
+
+void Board::DumpTileMapFromBoard()
+{
+	WorldManager::Get().GetGameObject<TileMap>("TileMap")->AddTilesInMap(Tiles_);
+}
+
+bool Board::IsAllSameRowLine(const int32_t& BoardYPosition, const Tile::EState& TileState)
+{
+	Vec2i BoardPosition;
+
+	for (int32_t BoardX = 1; BoardX < Width_ - 1; ++BoardX)
+	{
+		BoardPosition = Vec2i(BoardX, BoardYPosition);
+
+		const Tile& BoardTile = Tiles_[GetOffset(BoardPosition.x, BoardPosition.y, Width_, Height_)];
+
+		if (BoardTile.GetState() != TileState)
+		{
+			return false;
+		}
+	}
+
+	return true;
+}
+
+bool Board::CanRemoveRowLine(const int32_t& BoardYPosition)
+{
+	return IsAllSameRowLine(BoardYPosition, Tile::EState::FILL);
+}
+
+bool Board::IsEmptyRowLine(const int32_t& BoardYPosition)
+{
+	return IsAllSameRowLine(BoardYPosition, Tile::EState::EMPTY);
+}
+
+void Board::ClearRowLine(const int32_t& BoardYPosition)
+{
+	Vec2i BoardPosition;
+
+	for (int32_t BoardX = 1; BoardX < Width_ - 1; ++BoardX)
+	{
+		BoardPosition = Vec2i(BoardX, BoardYPosition);
+
+		Tile& BoardTile = Tiles_[GetOffset(BoardPosition.x, BoardPosition.y, Width_, Height_)];
+		BoardTile.SetState(Tile::EState::EMPTY);
+		BoardTile.SetColor(Tile::EColor::NONE);
+	}
+}
+
+void Board::ArrangeEmptyRowLine()
+{
+	std::vector<Tile> NewTiles = CreateBoardTile(TilePosition_, Width_, Height_);
+
+	for (int32_t BoardY = Height_ - 2, CurrentRowLine = Height_ - 2; BoardY >= 1; --BoardY)
+	{
+		if (!IsEmptyRowLine(BoardY))
+		{
+			for (int32_t BoardX = 1; BoardX < Width_ - 1; ++BoardX)
+			{
+				Tile& OriginTile = Tiles_[GetOffset(BoardX, BoardY, Width_, Height_)];
+				Tile& NewTile = NewTiles[GetOffset(BoardX, CurrentRowLine, Width_, Height_)];
+
+				NewTile.SetState(OriginTile.GetState());
+				NewTile.SetColor(OriginTile.GetColor());
+			}
+
+			CurrentRowLine--;
+		}
+	}
+
+	Tiles_ = NewTiles;
 }
--- Source/Public/Board.h
+++ Source/Public/Board.h
@@ -55,6 +55,22 @@ public:
 	virtual void Tick(float DeltaSeconds) override;
 
 
+	/**
+	 * @brief 보드의 상태를 얻습니다.
+	 *
+	 * @return 보드의 상태를 반환합니다.
+	 */
+	EState GetState() const { return State_; }
+
+
+	/**
+	 * @brief 보드의 상태를 설정합니다.
+	 *
+	 * @param State 설정할 보드의 상태입니다.
+	 */
+	void SetState(const EState& State) { State_ = State; }
+
+
 private:
 	/**
 	 * @brief 비어있는 테트리스 보드의 타일을 생성합니다.
@@ -66,6 +82,63 @@ private:
 	 * @return 생성된 테트리스 보드 타일의 벡터를 반환합니다.
 	 */
 	std::vector<Tile> CreateBoardTile(const Vec2i& TilePosition, const int32_t& Width, const int32_t& Height);
+
+
+	/**
+	 * @brief 타일 맵으로부터 보드 타일을 적제합니다.
+	 */
+	void DumpBoardFromTileMap();
+
+
+	/**
+	 * @brief 보드 타일을 타일 맵에 적재합니다.
+	 */
+	void DumpTileMapFromBoard();
+
+	
+	/**
+	 * @brief 보드의 세로 줄이 모두 같은 상태인지 확인합니다.
+	 * 
+	 * @param BoardYPosition 검사할 보드 상의 Y좌표입니다.
+	 * @param TileState 확인할 타일 상태입니다.
+	 * 
+	 * @return 보드의 세로 줄이 모두 같은 상태라면 true, 그렇지 않으면 false를 반환합니다.
+	 */
+	bool IsAllSameRowLine(const int32_t& BoardYPosition, const Tile::EState& TileState);
+
+
+	/**
+	 * @brief 보드의 세로 줄을 삭제할 수 있는지 확인합니다.
+	 * 
+	 * @param BoardYPosition 삭제할 수 있는지 확인할 보드 상의 Y좌표입니다.
+	 * 
+	 * @return 삭제할 수 있다면 true, 그렇지 않다면 false를 반환합니다.
+	 */
+	bool CanRemoveRowLine(const int32_t& BoardYPosition);
+
+
+	/**
+	 * @brief 보드의 세로 줄이 비어있는지 확인합니다.
+	 * 
+	 * @param BoardYPosition 비어있는지 확인할 보드 상의 Y좌표입니다.
+	 * 
+	 * @return 비어있다면 true, 그렇지 않다면 false를 반환합니다.
+	 */
+	bool IsEmptyRowLine(const int32_t& BoardYPosition);
+
+
+	/**
+	 * @brief 보드의 세로 줄을 삭제합니다.
+	 * 
+	 * @param BoardYPosition 삭제할 세로 줄입니다.
+	 */
+	void ClearRowLine(const int32_t& BoardYPosition);
+
+
+	/**
+	 * @brief 보드의 비어있는 줄을 재정렬합니다.
+	 */
+	void ArrangeEmptyRowLine();
 
 
 private:
@@ -97,4 +170,22 @@ private:
 	 * @brief 보드의 현재 상태입니다.
 	 */
 	EState State_ = EState::ACTIVE;
+
+
+	/**
+	 * @brief 보드가 삭제한 라인 수입니다.
+	 */
+	int32_t RemoveLine_ = 0;
+
+
+	/**
+	 * @brief 보드의 업데이트 누적 시간입니다.
+	 */
+	float AccrueTickTime_ = 0.0f;
+
+
+	/**
+	 * @brief 보드의 최대 업데이트 누적 시간입니다.
+	 */
+	static float MaxAccrueTickTime_;
 };
