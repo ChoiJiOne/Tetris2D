0876ace90f7a83b405c0f04a02ae66e33b504e9e
DESKTOP-4QAGCGF\choij <choijione02@gmail.com>
2023-01-29 20:22:45 +0900

    버튼 클릭 테스트 코드 삭제

--- Source/Private/StartScene.cpp
+++ Source/Private/StartScene.cpp
@@ -21,19 +21,4 @@ void StartScene::Tick(float DeltaSeconds)
 
 	WorldManager::Get().GetGameObject<Background>("Background")->Tick(DeltaSeconds);
 	WorldManager::Get().GetGameObject<GameTitle>("Title")->Tick(DeltaSeconds);
-
-	Texture2D& ButtonPush = ContentManager::Get().GetTexture2D("ButtonPush");
-	Texture2D& ButtonRelease = ContentManager::Get().GetTexture2D("ButtonRelease");
-
-	if (InputManager::Get().GetKeyPressState(EKeyCode::CODE_SPACE) == EPressState::HELD ||
-	    InputManager::Get().GetKeyPressState(EKeyCode::CODE_SPACE) == EPressState::PRESSED)
-	{
-		GraphicsManager::Get().DrawTexture2D(ButtonPush, Vec2f(0.0f, 0.0f), 100.0f, 20.0f);
-	}
-	
-	if (InputManager::Get().GetKeyPressState(EKeyCode::CODE_SPACE) == EPressState::RELEASED ||
-	    InputManager::Get().GetKeyPressState(EKeyCode::CODE_SPACE) == EPressState::NONE)
-	{
-		GraphicsManager::Get().DrawTexture2D(ButtonRelease, Vec2f(0.0f, 0.0f), 100.0f, 20.0f);
-	}
 }
