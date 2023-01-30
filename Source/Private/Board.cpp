7a8f3f96e6e17c1ab823025c744576c1c5b24941
DESKTOP-4QAGCGF\choij <choijione02@gmail.com>
2023-01-30 23:22:39 +0900

    보드의 최대 누적 시간 너무 빨라서 다시 변경

--- Source/Private/Board.cpp
+++ Source/Private/Board.cpp
@@ -8,7 +8,7 @@
 #include "Utility.hpp"
 #include "WorldManager.h"
 
-float Board::MaxAccrueTickTime_ = 0.4f;
+float Board::MaxAccrueTickTime_ = 0.6f;
 
 Board::Board(const std::string& Signature, const Vec2i& TilePosition, const int32_t& Width, const int32_t& Height)
 	: GameObject(Signature)
