b312c6aae91a9aafbc09adf75a4118ff9fe8285b
DESKTOP-N6FOOG4\choij <choijione02@gmail.com>
2023-01-30 13:25:01 +0900

    게임 내의 버튼 기능을 수행하는 Button class 선언 및 구현
    - 올바르지 않은 위치에 있는 소스 파일 위치 변경

---
+++ Source/Private/Button.cpp
@@ -0,0 +1,140 @@
+#include "Button.h"
+#include "ContentManager.h"
+#include "Font.h"
+#include "Texture2D.h"
+#include "GraphicsManager.h"
+#include "Shader.h"
+#include "WorldManager.h"
+
+Button::Button(const std::string& Signature, const ButtonParamWithText& ConstructorParam)
+	: Button(
+		Signature,
+		ConstructorParam.Position,
+		ConstructorParam.Width,
+		ConstructorParam.Height,
+		ConstructorParam.TextureSignature,
+		ConstructorParam.Text,
+		ConstructorParam.Color,
+		ConstructorParam.FontSignature,
+		ConstructorParam.KeyCode,
+		ConstructorParam.ClickEvent,
+		ConstructorParam.ReduceRate
+	) {}
+
+Button::Button(
+	const std::string& Signature, 
+	const Vec2f& Position, 
+	const float& Width, 
+	const float& Height,
+	const std::string& TextureSignature,
+	const std::wstring& Text,
+	const LinearColor& Color,
+	const std::string& FontSignature, 
+	const EKeyCode& KeyCode, 
+	const std::function<void()>& ClickEvent, 
+	const float ReduceRate
+) : GameObject(Signature),
+	bIsTextMode_(true),
+	Position_(Position),
+	Width_(Width),
+	Height_(Height),
+	TextureSignature_(TextureSignature),
+	Text_(Text),
+	Color_(Color),
+	FontSignature_(FontSignature),
+	KeyCode_(KeyCode),
+	ClickEvent_(ClickEvent),
+	ReduceRate_(ReduceRate)
+{
+}
+
+Button::Button(const std::string& Signature, const ButtonParamWithoutText& ConstructorParam)
+	: Button(
+		Signature,
+		ConstructorParam.Position,
+		ConstructorParam.Width,
+		ConstructorParam.Height,
+		ConstructorParam.TextureSignature,
+		ConstructorParam.KeyCode,
+		ConstructorParam.ClickEvent,
+		ConstructorParam.ReduceRate
+	) {}
+
+Button::Button(
+	const std::string& Signature, 
+	const Vec2f& Position, 
+	const float& Width, 
+	const float& Height,
+	const std::string& TextureSignature,
+	const EKeyCode& KeyCode, 
+	const std::function<void()>& ClickEvent,
+	const float ReduceRate
+) : GameObject(Signature),
+	bIsTextMode_(false),
+	Position_(Position),
+	Width_(Width),
+	Height_(Height),
+	TextureSignature_(TextureSignature),
+	KeyCode_(KeyCode),
+	ClickEvent_(ClickEvent),
+	ReduceRate_(ReduceRate)
+{
+}
+
+Button::~Button()
+{
+}
+
+void Button::Tick(float DeltaSeconds)
+{
+	bIsDetectMouse_ = IsDetectMouse();
+
+	float ButtonTextureWidth = Width_;
+	float ButtonTextureHeight = Height_;
+
+	if (bIsDetectMouse_)
+	{
+		const EPressState& PressState = InputManager::Get().GetKeyPressState(KeyCode_);
+
+		if (PressState == EPressState::HELD || PressState == EPressState::PRESSED)
+		{
+			ButtonTextureWidth *= ReduceRate_;
+			ButtonTextureHeight *= ReduceRate_;
+		}
+
+		if (PressState == EPressState::RELEASED && ClickEvent_)
+		{
+			ClickEvent_();
+		}
+	}
+
+	Texture2D& ButtonTexture = ContentManager::Get().GetTexture2D(TextureSignature_);
+	GraphicsManager::Get().DrawTexture2D(ButtonTexture, Position_, ButtonTextureWidth, ButtonTextureHeight);
+
+	if (bIsTextMode_)
+	{
+		Font& ButtonFont = ContentManager::Get().GetFont(FontSignature_);
+		GraphicsManager::Get().DrawText2D(ButtonFont, Text_, Position_, Color_);
+	}
+}
+
+bool Button::IsDetectMouse() const
+{
+	Vec2i MousePosition = InputManager::Get().GetCurrMousePositionFromWindow();
+
+	Vec2f WindowMousePosition = Vec2f(static_cast<float>(MousePosition.x), static_cast<float>(MousePosition.y));
+	Vec2f CameraPositon = WorldManager::Get().GetMainCamera().GetPosition();
+	float CameraWidth = WorldManager::Get().GetMainCamera().GetWidth();
+	float CameraHeight = WorldManager::Get().GetMainCamera().GetHeight();
+	
+	Vec2f MousePositionFromWorld = Vec2f(
+		+WindowMousePosition.x + CameraPositon.x - CameraWidth / 2.0f,
+		-WindowMousePosition.y + CameraPositon.y + CameraHeight / 2.0f
+	);
+
+	float DiffX = std::abs(Position_.x - MousePositionFromWorld.x);
+	float DiffY = std::abs(Position_.y - MousePositionFromWorld.y);
+
+	return (0.0f <= DiffX && DiffX <= Width_ / 2.0f)
+		&& (0.0f <= DiffY && DiffY <= Height_ / 2.0f);
+}
--- Source/Public/Button.h
+++ Source/Public/Button.h
@@ -1,9 +1,46 @@
 #pragma once
 
+#include "Color.h"
 #include "Vector.hpp"
 #include "GameObject.h"
+#include "InputManager.h"
 
 #include <functional>
+
+
+/**
+ * @brief 텍스트가 포함된 버튼 생성자 파라미터입니다.
+ * 
+ * @note 버튼에 표시되는 텍스트는 UTF-16 기반입니다.
+ */
+struct ButtonParamWithText
+{
+	const Vec2f& Position;
+	const float& Width;
+	const float& Height;
+	const std::string& TextureSignature;
+	const std::wstring& Text;
+	const LinearColor& Color;
+	const std::string& FontSignature;
+	const EKeyCode& KeyCode;
+	const std::function<void()>& ClickEvent;
+	const float ReduceRate = 1.0f;
+};
+
+
+/**
+ * @brief 텍스트가 포함되지 않은 버튼 생성자 파라미터입니다.
+ */
+struct ButtonParamWithoutText
+{
+	const Vec2f& Position;
+	const float& Width;
+	const float& Height;
+	const std::string& TextureSignature;
+	const EKeyCode& KeyCode;
+	const std::function<void()>& ClickEvent;
+	const float ReduceRate = 1.0f;
+};
 
 
 /**
@@ -14,19 +51,76 @@ class Button : public GameObject
 public:
 	/**
 	 * @brief 게임 내의 텍스트가 포함된 사각형 버튼 클래스의 생성자입니다.
+	 *
+	 * @param Signature 사각형 버튼의 시그니처 값입니다.
+	 * @param ConstructorParam 게임 내의 텍스트가 포함된 사각형 버튼 클래스의 생성 파라미터입니다.
+	 */
+	Button(const std::string& Signature, const ButtonParamWithText& ConstructorParam);
+
+
+	/**
+	 * @brief 게임 내의 텍스트가 포함된 사각형 버튼 클래스의 생성자입니다.
+	 * 
+	 * @note 버튼에 표시되는 텍스트는 UTF-16 기반입니다.
 	 * 
 	 * @param Signature 사각형 버튼의 시그니처 값입니다.
+	 * @param Position 월드 상의 버튼의 위치입니다.
+	 * @param Width 버튼의 가로 크기입니다.
+	 * @param Height 버튼의 세로 크기입니다.
+	 * @param TextureSignature 버튼의 텍스처 시그니처입니다.
+	 * @param Text 버튼에 표시될 텍스트입니다.
+	 * @param Color 버튼에 표시될 텍스트의 색상입니다.
+	 * @param FontSignature 버튼에 표시될 텍스트의 폰트 시그니처입니다.
+	 * @param KeyCode 버튼이 클릭됐을 때 실행할 키 코드 값입니다.
+	 * @param ClickEvent 버튼이 클릭됐을 때 실행할 이벤트입니다.
+	 * @param ReduceRate 버튼이 클릭됐을 때 줄어들 비율입니다. 기본 값은 1.0입니다.
 	 */
-	Button(const std::string& Signature);
-
+	Button(
+		const std::string& Signature,
+		const Vec2f& Position,
+		const float& Width,
+		const float& Height,
+		const std::string& TextureSignature,
+		const std::wstring& Text,
+		const LinearColor& Color,
+		const std::string& FontSignature,
+		const EKeyCode& KeyCode,
+		const std::function<void()>& ClickEvent,
+		const float ReduceRate = 1.0f
+	);
 
 
 	/**
 	 * @brief 게임 내의 텍스트가 포함되지 않은 사각형 버튼 클래스의 생성자입니다.
-	 * 
-	 * 
+	 *
+	 * @param Signature 사각형 버튼의 시그니처 값입니다.
+	 * @param ConstructorParam 게임 내의 텍스트가 포함되지 않은 사각형 버튼 클래스의 생성 파라미터입니다.
 	 */
+	Button(const std::string& Signature, const ButtonParamWithoutText& ConstructorParam);
+	
 
+	/**
+	 * @brief 게임 내의 텍스트가 포함되지 않은 사각형 버튼 클래스의 생성자입니다.
+	 * 
+	 * @param Signature 사각형 버튼의 시그니처 값입니다.
+	 * @param Position 월드 상의 버튼의 위치입니다.
+	 * @param Width 버튼의 가로 크기입니다.
+	 * @param Height 버튼의 세로 크기입니다.
+	 * @param TextureSignature 버튼의 텍스처 시그니처입니다.
+	 * @param KeyCode 버튼이 클릭됐을 때 실행할 키 코드 값입니다.
+	 * @param ClickEvent 버튼이 클릭됐을 때 실행할 이벤트입니다.
+	 * @param ReduceRate 버튼이 클릭됐을 때 줄어들 비율입니다. 기본 값은 1.0입니다.
+	 */
+	Button(
+		const std::string& Signature,
+		const Vec2f& Position,
+		const float& Width,
+		const float& Height,
+		const std::string& TextureSignature,
+		const EKeyCode& KeyCode,
+		const std::function<void()>& ClickEvent,
+		const float ReduceRate = 1.0f
+	);
 
 
 	/**
@@ -41,8 +135,36 @@ public:
 	DISALLOW_COPY_AND_ASSIGN(Button);
 
 
+	/**
+	 * @brief 버튼을 업데이트합니다.
+	 * 
+	 * @param DeltaSeconds 초단위 델타 시간값입니다.
+	 */
+	virtual void Tick(float DeltaSeconds) override;
+
 
 private:
+	/**
+	 * @brief 마우스가 범위 내에 있는지 확인합니다.
+	 * 
+	 * @return 마우스가 범위 내에 있다면 true, 그렇지 않으면 false를 반환합니다.
+	 */
+	bool IsDetectMouse() const;
+
+
+private:
+	/**
+	 * @brief 마우스가 버튼 내에 있는지 확인합니다.
+	 */
+	bool bIsDetectMouse_ = false;
+
+
+	/**
+	 * @brief 버튼 내에 텍스트가 있는지 확인합니다.
+	 */
+	bool bIsTextMode_ = false;
+
+
 	/**
 	 * @brief 월드 상의 사각형 버튼 중심 좌표입니다.
 	 */
@@ -62,11 +184,17 @@ private:
 
 
 	/**
+	 * @brief 버튼의 텍스처 시그니처입니다.
+	 */
+	std::string TextureSignature_;
+
+
+	/**
 	 * @brief 사각형 버튼의 텍스트입니다.
 	 * 
 	 * @note 텍스트가 없다면 무시됩니다.
 	 */
-
+	std::wstring Text_;
 
 
 	/**
@@ -74,10 +202,29 @@ private:
 	 * 
 	 * @note 텍스트가 없다면 무시됩니다.
 	 */
-
+	LinearColor Color_;
 
 
 	/**
-	 * @brief 
+	 * @brief 사각형 텍스트의 폰트입니다.
 	 */
+	std::string FontSignature_;
+
+
+	/**
+	 * @brief 버튼에 대응하는 키 코드 값입니다.
+	 */
+	EKeyCode KeyCode_ = EKeyCode::CODE_NONE;
+
+
+	/**
+	 * @brief 버튼 클릭 시 발생할 이벤트입니다.
+	 */
+	std::function<void()> ClickEvent_ = nullptr;
+
+
+	/**
+	 * @brief 버튼 클릭 시 줄어드는 버튼의 비율입니다.
+	 */
+	float ReduceRate_ = 0.0f;
 };
