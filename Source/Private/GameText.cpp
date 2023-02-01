d53462e7a2c840f57ff5a7d23071f51eac35dcc6
DESKTOP-N6FOOG4\choij <choijione02@gmail.com>
2023-02-01 12:23:45 +0900

    좀 더 넓은 범위에서 사용하기 위해 GameTitle을 GameText로 이름 변경

--- Source/Private/GameText.cpp
+++ Source/Private/GameText.cpp
@@ -1,26 +1,26 @@
-#include "GameTitle.h"
+#include "GameText.h"
 #include "ContentManager.h"
 #include "Font.h"
 #include "GraphicsManager.h"
 #include "Shader.h"
 
-GameTitle::GameTitle(
+GameText::GameText(
 	const std::string& Signature, 
-	const std::wstring& Title, 
+	const std::wstring& Text, 
 	const std::string& FontSignature,
 	const Vec2f& Position, 
 	const LinearColor& Color
 )
 	: GameObject(Signature)
-	, Title_(Title)
+	, Text_(Text)
 	, FontSignature_(FontSignature)
 	, Position_(Position)
 	, Color_(Color)
 {
 }
 
-void GameTitle::Tick(float DeltaSeconds)
+void GameText::Tick(float DeltaSeconds)
 {
 	Font& TitleFont = ContentManager::Get().GetFont(FontSignature_);
-	GraphicsManager::Get().DrawText2D(TitleFont, Title_, Position_, Color_);
+	GraphicsManager::Get().DrawText2D(TitleFont, Text_, Position_, Color_);
 }
