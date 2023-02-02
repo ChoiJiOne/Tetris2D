1980afbc6c32da290e3969278f19736a3b56f5cb
DESKTOP-N6FOOG4\choij <choijione02@gmail.com>
2023-02-03 08:48:56 +0900

    설계 방향 변경으로 기존의 게임 컨텐츠 관련 코드 삭제

--- Game/Source/Launch.cpp
+++ Game/Source/Launch.cpp
@@ -1,6 +1,3 @@
-#include "Background.h"
-#include "Button.h"
-#include "Color.h"
 #include "GameEngine.h"
 #include "GraphicsManager.h"
 #include "GameText.h"
@@ -9,12 +6,6 @@
 #include "Timer.h"
 #include "Window.h"
 #include "WorldManager.h"
-
-#include "StartScene.h"
-#include "DoneScene.h"
-#include "SettingScene.h"
-#include "PlayScene.h"
-#include "PauseScene.h"
 
 
 /**
@@ -34,10 +25,6 @@ public:
 	 */
 	virtual ~Tetris()
 	{
-		for (auto& GameScene : Scenes_)
-		{
-			GameScene.second.reset();
-		}
 	}
 
 
@@ -51,10 +38,10 @@ public:
 		GameEngine::Initialize();
 
 		RegisterWindowEvent();
-		CreateBasicGameObjectAndCamera();
-		CreateScenesAndSwitchEvents();
 
-		CurrentScene_ = Scenes_["Start"].get();
+		float Width = 0.0f, Height = 0.0f;
+		GraphicsManager::Get().GetBackBufferSize(Width, Height);
+		WorldManager::Get().CreateMainCamera(Vec2f(0.0f, 0.0f), Width, Height);
 	}
 
 
@@ -74,8 +61,6 @@ public:
 
 			GraphicsManager::Get().Clear(BLACK);
 
-			CurrentScene_->Tick(Timer_.GetDeltaTime());
-
 			GraphicsManager::Get().Present();
 		}
 
@@ -90,13 +75,7 @@ private:
 	void RegisterWindowEvent()
 	{
 		std::function<void()> CloseEvent = [&]() { bIsDone_ = true; };
-		std::function<void()> InactiveEvent = [&]() { 
-			PlayScene* PlayScenePtr = reinterpret_cast<PlayScene*>(Scenes_["Play"].get());
-			if (CurrentScene_ == PlayScenePtr)
-			{
-				CurrentScene_ = Scenes_["Pause"].get();
-			}
-		};
+		std::function<void()> InactiveEvent = [&]() { };
 		std::function<void()> ResizeEvent = [&]() {
 			GraphicsManager::Get().Resize();
 			float Width = 0.0f, Height = 0.0f;
@@ -120,131 +99,6 @@ private:
 	}
 
 
-	/**
-	 * @brief 기본적인 게임 오브젝트와 카메라를 생성합니다.
-	 */
-	void CreateBasicGameObjectAndCamera()
-	{
-		float Width = 0.0f, Height = 0.0f;
-		GraphicsManager::Get().GetBackBufferSize(Width, Height);
-		WorldManager::Get().CreateMainCamera(Vec2f(0.0f, 0.0f), Width, Height);
-
-		WorldManager::Get().CreateGameObject<GameText>("Title", L"TETRIS 2D", "Font128", Vec2f(0.0f, 250.0f), CYAN);
-		WorldManager::Get().CreateGameObject<Background>("Background");
-	}
-
-
-	/**
-	 * @brief 게임 내의 씬과 이벤트를 생성합니다.
-	 */
-	void CreateScenesAndSwitchEvents()
-	{
-		std::function<void()> StartSceneSwitchEvent = [&]() {
-			StartScene* StartScenePtr = reinterpret_cast<StartScene*>(Scenes_["Start"].get());
-			StartScene::EState State = StartScenePtr->GetCurrentState();
-
-			switch (State)
-			{
-			case StartScene::EState::START:
-				CurrentScene_ = Scenes_["Play"].get();
-				reinterpret_cast<PlayScene*>(CurrentScene_)->Reset();
-				break;
-
-			case StartScene::EState::SETTING:
-				CurrentScene_ = Scenes_["Setting"].get();
-				break;
-
-			case StartScene::EState::QUIT:
-				bIsDone_ = true;
-				break;
-			}
-		};
-
-		std::function<void()> SettingSceneSwitchEvent = [&]() {
-			SettingScene* SettingScenePtr = reinterpret_cast<SettingScene*>(Scenes_["Setting"].get());
-			SettingScene::EState State = SettingScenePtr->GetCurrentState();
-
-			switch (State)
-			{
-			case SettingScene::EState::BACK:
-				CurrentScene_ = Scenes_["Start"].get();
-			}
-		};
-
-		std::function<void()> PlaySceneSwitchEvent = [&]() {
-			PlayScene* PlayScenePtr = reinterpret_cast<PlayScene*>(Scenes_["Play"].get());
-			PlayScene::EState State = PlayScenePtr->GetCurrentState();
-
-			switch (State)
-			{
-			case PlayScene::EState::PAUSE:
-				CurrentScene_ = Scenes_["Pause"].get();
-				break;
-
-			case PlayScene::EState::DONE:
-				CurrentScene_ = Scenes_["Done"].get();
-				break;
-			}
-		};
-
-		std::function<void()> PauseSceneSwitchEvent = [&]() {
-			PauseScene* PauseScenePtr = reinterpret_cast<PauseScene*>(Scenes_["Pause"].get());
-			PauseScene::EState State = PauseScenePtr->GetCurrentState();
-
-			switch (State)
-			{
-			case PauseScene::EState::CONTINUE:
-				CurrentScene_ = Scenes_["Play"].get();
-				break;
-
-			case PauseScene::EState::RESET:
-				CurrentScene_ = Scenes_["Start"].get();
-				break;
-
-			case PauseScene::EState::QUIT:
-				bIsDone_ = true;
-				break;
-			}
-		};
-
-		std::function<void()> DoneSceneSwitchEvent = [&]() {
-			DoneScene* DoneScenePtr = reinterpret_cast<DoneScene*>(Scenes_["Done"].get());
-			DoneScene::EState SelectState = DoneScenePtr->GetCurrentState();
-
-			switch (SelectState)
-			{
-			case DoneScene::EState::REPLAY:
-				CurrentScene_ = Scenes_["Play"].get();
-				reinterpret_cast<PlayScene*>(CurrentScene_)->Reset();
-				break;
-
-			case DoneScene::EState::RESET:
-				CurrentScene_ = Scenes_["Start"].get();
-				break;
-
-			case DoneScene::EState::QUIT:
-				bIsDone_ = true;
-				break;
-			}
-		};
-
-		Scenes_["Start"] = std::make_unique<StartScene>();
-		Scenes_["Start"]->SetSwitchEvent(StartSceneSwitchEvent);
-
-		Scenes_["Setting"] = std::make_unique<SettingScene>();
-		Scenes_["Setting"]->SetSwitchEvent(SettingSceneSwitchEvent);
-
-		Scenes_["Play"] =  std::make_unique<PlayScene>();
-		Scenes_["Play"]->SetSwitchEvent(PlaySceneSwitchEvent);
-
-		Scenes_["Pause"] = std::make_unique<PauseScene>();
-		Scenes_["Pause"]->SetSwitchEvent(PauseSceneSwitchEvent);
-
-		Scenes_["Done"] =  std::make_unique<DoneScene>();
-		Scenes_["Done"]->SetSwitchEvent(DoneSceneSwitchEvent);
-	}
-
-
 private:
 	/**
 	 * @brief 게임 타이머입니다.
@@ -252,16 +106,16 @@ private:
 	Timer Timer_;
 
 
-	/**
-	 * @brief 현재 실행 중인 씬입니다.
-	 */
-	Scene* CurrentScene_ = nullptr;
+	///**
+	// * @brief 현재 실행 중인 씬입니다.
+	// */
+	//Scene* CurrentScene_ = nullptr;
 
 
-	/**
-	 * @brief 테트리스 게임 내의 씬입니다.
-	 */
-	std::unordered_map<std::string, std::unique_ptr<Scene>> Scenes_;
+	///**
+	// * @brief 테트리스 게임 내의 씬입니다.
+	// */
+	//std::unordered_map<std::string, std::unique_ptr<Scene>> Scenes_;
 };
 
 
