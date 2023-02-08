edb503b45fb65595abe88241517f401f69de25e7
DESKTOP-4QAGCGF\choij <choijione02@gmail.com>
2023-02-08 21:03:37 +0900

    테트로미노의 입력 처리 컴포넌트에서 다음 이동 방향 및 누적 시간을 관리하도록 수정

--- Game/Source/Private/TetrominoInputComponent.cpp
+++ Game/Source/Private/TetrominoInputComponent.cpp
@@ -1,28 +1,26 @@
 #include "TetrominoInputComponent.h"
 #include "BlockComponent.h"
 #include "InputManager.h"
-#include "Tetromino.h"
 
 static const std::unordered_map<EKeyCode, Tetromino::EDirection> KEY_DIRECTIONS = {
-	{ EKeyCode::CODE_LEFT,  Tetromino::EDirection::LEFT  },
-	{ EKeyCode::CODE_RIGHT, Tetromino::EDirection::RIGHT },
-	{ EKeyCode::CODE_UP,    Tetromino::EDirection::CW    },
-	{ EKeyCode::CODE_DOWN,  Tetromino::EDirection::DOWN  },
+	{ EKeyCode::CODE_LEFT,   Tetromino::EDirection::LEFT  },
+	{ EKeyCode::CODE_RIGHT,  Tetromino::EDirection::RIGHT },
+	{ EKeyCode::CODE_UP,     Tetromino::EDirection::CW    },
+	{ EKeyCode::CODE_DOWN,   Tetromino::EDirection::DOWN  },
+	{ EKeyCode::CODE_SPACE,  Tetromino::EDirection::JUMP  },
 };
 
 void TetrominoInputComponent::Tick(float DeltaSecons)
 {
 	AccrueFrameTime_ += DeltaSecons;
 
-	Tetromino::EDirection Direction = Tetromino::EDirection::NONE;
-	
+	Direction_ = Tetromino::EDirection::NONE;
+
 	for (const auto& KeyDirection : KEY_DIRECTIONS)
 	{
 		if (InputManager::Get().GetKeyPressState(KeyDirection.first) == EPressState::PRESSED)
 		{
-			Direction = KeyDirection.second;
+			Direction_ = KeyDirection.second;
 		}
 	}
-
-	reinterpret_cast<Tetromino*>(GetGameObject())->Move(Direction);
 }
--- Game/Source/Public/TetrominoInputComponent.h
+++ Game/Source/Public/TetrominoInputComponent.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "Tetromino.h"
 #include "InputComponent.h"
 
 
@@ -25,11 +26,43 @@ public:
 
 
 	/**
+	 * @brief 컴포넌트의 복사 생성자 및 대입연산자를 삭제합니다.
+	 */
+	DISALLOW_COPY_AND_ASSIGN(TetrominoInputComponent);
+
+
+	/**
 	 * @brief 테트로미노의 입력 처리 컴포넌트를 업데이트합니다.
 	 * 
 	 * @param DeltaSecons 초단위 델타 시간값입니다.
 	 */
 	void Tick(float DeltaSecons);
+
+	
+	/**
+	 * @brief 프레임 누적 시간을 얻습니다.
+	 * 
+	 * @note 시간은 초단위 입니다.
+	 * 
+	 * @return 프레임 누적 시간을 반환합니다.
+	 */
+	float GetAccrueFrameTime() const { return AccrueFrameTime_; }
+
+
+	/**
+	 * @brief 프레임 누적 시간을 초기화합니다.
+	 * 
+	 * @note 누적 시간값을 0으로 만듭니다.
+	 */
+	void ResetAccrueFrameTime() { AccrueFrameTime_ = 0.0f; }
+
+	
+	/**
+	 * @brief 테트로미노의 이동 방향을 얻습니다.
+	 * 
+	 * @return 테트로미노의 이동 방향을 반환합니다.
+	 */
+	Tetromino::EDirection GetDirection() const { return Direction_; }
 
 
 private:
@@ -37,4 +70,10 @@ private:
 	 * @brief 프레임 누적 시간입니다.
 	 */
 	float AccrueFrameTime_ = 0.0f;
+
+
+	/**
+	 * @brief 테트로미노의 이동 방향입니다.
+	 */
+	Tetromino::EDirection Direction_ = Tetromino::EDirection::NONE;
 };
