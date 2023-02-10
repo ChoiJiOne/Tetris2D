d5aee0d43a8e15ae1e036b5f6f07cd05d2187458
DESKTOP-4QAGCGF\choij <choijione02@gmail.com>
2023-02-10 16:14:18 +0900

    블록의 상태를 나타내는 enum class 및 멤버 변수 추가
    - 블록에 상태에 따라 충돌 처리를 새롭게 처리하도록 변경
    - 블록의 타입(EType)을 색상(EColor)으로 변경

--- Game/Source/Private/BlockComponent.cpp
+++ Game/Source/Private/BlockComponent.cpp
@@ -1,13 +1,23 @@
 #include "BlockComponent.h"
 
-std::unordered_map<BlockComponent::EType, std::string> BlockComponent::TypeTextureSignatures_ = {
-	{ BlockComponent::EType::BLUE,   "Blue"   },
-	{ BlockComponent::EType::CYAN,   "Cyan"   },
-	{ BlockComponent::EType::GRAY,   "Gray"   },
-	{ BlockComponent::EType::GREEN,  "Green"  },
-	{ BlockComponent::EType::ORANGE, "Orange" },
-	{ BlockComponent::EType::PINK,   "Pink"   },
-	{ BlockComponent::EType::PURPLE, "Purple" },
-	{ BlockComponent::EType::RED,    "Red"    },
-	{ BlockComponent::EType::YELLOW, "Yellow" },
+std::unordered_map<BlockComponent::EColor, std::string> BlockComponent::ColorTextureSignatures_ = {
+	{ BlockComponent::EColor::BLUE,   "Blue"   },
+	{ BlockComponent::EColor::CYAN,   "Cyan"   },
+	{ BlockComponent::EColor::GRAY,   "Gray"   },
+	{ BlockComponent::EColor::GREEN,  "Green"  },
+	{ BlockComponent::EColor::ORANGE, "Orange" },
+	{ BlockComponent::EColor::PINK,   "Pink"   },
+	{ BlockComponent::EColor::PURPLE, "Purple" },
+	{ BlockComponent::EColor::RED,    "Red"    },
+	{ BlockComponent::EColor::YELLOW, "Yellow" },
 };
+
+bool BlockComponent::IsCollision(const BlockComponent* BoundingBox)
+{
+	if (State_ == EState::EMPTY || BoundingBox->GetState() == EState::EMPTY)
+	{
+		return false;
+	}
+
+	return AABBComponent::IsCollision(BoundingBox);
+}
--- Game/Source/Public/BlockComponent.h
+++ Game/Source/Public/BlockComponent.h
@@ -14,9 +14,22 @@ class BlockComponent : public AABBComponent
 {
 public:
 	/**
-	 * @brief 테트리스 블럭의 타입입니다.
+	 * @brief 테트리스 블럭의 상태입니다.
+	 *
+	 * @note 테트리스 블럭의 상태를 기반으로 충돌 처리 및 렌더링이 수행됩니다.
 	 */
-	enum class EType : int32_t
+	enum class EState : int32_t
+	{
+		NONE  = 0,
+		EMPTY = 1,
+		FILL  = 2,
+	};
+
+
+	/**
+	 * @brief 테트리스 블럭의 색상입니다.
+	 */
+	enum class EColor : int32_t
 	{
 		NONE   = 0,
 		BLUE   = 1,
@@ -33,16 +46,18 @@ public:
 
 public:
 	/**
-	 * @brief 게임 오브젝트가 소유한 테트리스 블럭의 생성자입니다.
+	 * @brief 게임 오브젝트가 소유한 테트리스 블럭의 컴포넌트 생성자입니다.
 	 * 
 	 * @param Object 이 컴포넌트를 소유하는 오브젝트의 포인터 값입니다.
-	 * @param Center 블럭의 중심 좌표입니다.
-	 * @param Side 정사각형 블럭의 한 변의 길이입니다.
-	 * @param Type 블럭의 타입입니다.
+	 * @param Center 테트리스 블럭의 중심 좌표입니다.
+	 * @param Side 테트리스 블럭의 한 변의 길이입니다.
+	 * @param Color 테트리스 블럭의 색상입니다.
+	 * @param State 블럭의 상태입니다.
 	 */
-	BlockComponent(GameObject* Object, const Vec2f& Center, const float& Side, const EType& Type)
+	BlockComponent(GameObject* Object, const Vec2f& Center, const float& Side, const EColor& Color, const EState& State)
 		: AABBComponent(Object, Center, Side, Side)
-		, Type_(Type) {}
+		, State_(State) 
+		, Color_(Color) {}
 
 
 	/**
@@ -58,11 +73,45 @@ public:
 
 
 	/**
-	 * @brief 테트리스 블럭의 타입을 얻습니다.
+	 * @brief 블럭의 충돌을 검사합니다.
 	 * 
-	 * @return 테트리스 블럭의 타입을 반환합니다.
+	 * @param BoundingBox 검사를 수행할 블럭입니다.
+	 * 
+	 * @return 충돌한다면 true, 그렇지 않다면 false를 반환합니다.
 	 */
-	EType GetType() const { return Type_; }
+	bool IsCollision(const BlockComponent* BoundingBox);
+
+
+	/**
+	 * @brief 테트리스 블럭의 크기를 얻습니다.
+	 * 
+	 * @return 테트리스 블럭의 크기를 반환합니다.
+	 */
+	float GetSide() const { return GetWidth(); }
+
+
+	/**
+	 * @brief 테트리스 블럭의 상태를 얻습니다.
+	 * 
+	 * @return 테트리스 블럭의 상태를 반환합니다.
+	 */
+	EState GetState() const { return State_; }
+
+
+	/**
+	 * @brief 테트리스 블럭의 상태를 설정합니다.
+	 * 
+	 * @param State 설정할 테트리스 블럭의 상태입니다.
+	 */
+	void SetState(const EState& State) { State_ = State; }
+
+
+	/**
+	 * @brief 테트리스 블럭의 색상을 얻습니다.
+	 * 
+	 * @return 테트리스 블럭의 색상을 반환합니다.
+	 */
+	EColor GetColor() const { return Color_; }
 
 
 	/**
@@ -70,18 +119,24 @@ public:
 	 * 
 	 * @return 테트리스 블럭 타입에 대응하는 텍스처 시그니처 값을 반환합니다.
 	 */
-	const std::string& GetTypeTextureSignature() const { return TypeTextureSignatures_[Type_]; };
+	const std::string& GetColorTextureSignature() const { return ColorTextureSignatures_[Color_]; };
 	
 
 private:
 	/**
-	 * @brief 테트리스 블럭의 타입입니다.
+	 * @brief 테트리스 블럭의 상태입니다.
 	 */
-	EType Type_ = EType::NONE;
+	EState State_ = EState::NONE;
 
 
 	/**
-	 * @brief 테트리스 블럭의 타입에 대응하는 텍스처의 시그니처 값입니다.
+	 * @brief 테트리스 블럭의 색상입니다.
 	 */
-	static std::unordered_map<EType, std::string> TypeTextureSignatures_;
+	EColor Color_ = EColor::NONE;
+
+
+	/**
+	 * @brief 테트리스 블럭의 색상에 대응하는 텍스처의 시그니처 값입니다.
+	 */
+	static std::unordered_map<EColor, std::string> ColorTextureSignatures_;
 };
