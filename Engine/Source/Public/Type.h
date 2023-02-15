091ebc8dd29c6bba2a54f8fa6c0528e13f41c0d2
DESKTOP-N6FOOG4\choij <choijione02@gmail.com>
2023-02-16 08:32:03 +0900

    크기가 명시된 정수 자료형 선언

---
+++ Engine/Source/Public/Type.h
@@ -0,0 +1,24 @@
+#pragma once
+
+
+/**
+ * @brief 크기가 명시된 정수 자료형을 선언합니다.
+ *
+ * @see
+ * int8 https://docs.unrealengine.com/4.27/en-US/API/Runtime/Core/HAL/int8/
+ * int16 https://docs.unrealengine.com/4.27/en-US/API/Runtime/Core/HAL/int16/
+ * int32 https://docs.unrealengine.com/4.27/en-US/API/Runtime/Core/HAL/int32/
+ * int64 https://docs.unrealengine.com/4.27/en-US/API/Runtime/Core/HAL/int64/
+ * uint8 https://docs.unrealengine.com/4.27/en-US/API/Runtime/Core/HAL/uint8/
+ * uint16 https://docs.unrealengine.com/4.27/en-US/API/Runtime/Core/HAL/uint16/
+ * uint32 https://docs.unrealengine.com/4.27/en-US/API/Runtime/Core/HAL/uint32/
+ * uint64 https://docs.unrealengine.com/4.27/en-US/API/Runtime/Core/HAL/uint64/
+ */
+using int8   = signed char;
+using int16  = short;
+using int32  = int;
+using int64  = long long;
+using uint8  = unsigned char;
+using uint16 = unsigned short;
+using uint32 = unsigned int;
+using uint64 = unsigned long long;
