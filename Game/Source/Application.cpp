bc28b15501def17b3a9fa0e47558a1e660c4903b
DESKTOP-N6FOOG4\choij <choijione02@gmail.com>
2023-02-15 15:31:43 +0900

    Windows Platform에서 동작하는 빈 메인 함수 추가

---
+++ Game/Source/Application.cpp
@@ -0,0 +1,18 @@
+#include <windows.h>
+
+/**
+ * @brief Windows 애플리케이션 진입점입니다.
+ *
+ * @see https://learn.microsoft.com/ko-kr/windows/win32/learnwin32/winmain--the-application-entry-point
+ *
+ * @param hInstance 인스턴스에 대한 핸들입니다.
+ * @param hPrevInstance 아무 의미 없는 파라미터입니다.
+ * @param CmdLine 명령줄 인수입니다.
+ * @param CmdShow 기본 애플리케이션 창을 최소화할지, 최대화할지 또는 정상적으로 표시할지를 나타내는 플래그입니다.
+ *
+ * @return 다른 프로그램에 전달할 수 있는 상태 코드를 반환합니다.
+ */
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR CmdLine, int CmdShow)
+{
+	return 0;
+}
