b94a0daab87e6d99c22f318bcfa73cc3cd99c654
DESKTOP-N6FOOG4\choij <choijione02@gmail.com>
2023-02-17 13:35:37 +0900

    윈도우 이벤트 처리 기능 구현 테스트

--- Game/Source/Application.cpp
+++ Game/Source/Application.cpp
@@ -1,43 +1,10 @@
 #include "Type.h"
+#include "WindowEvent.h"
 
 #include <string>
 #include <windows.h>
 
 #pragma warning(disable: 28251)
-
-
-/**
- * @brief 윈도우 메시지 핸들러입니다.
- *
- * @note 내부의 윈도우 메시지 핸들러를 호출합니다.
- *
- * @see https://learn.microsoft.com/ko-kr/windows/win32/learnwin32/writing-the-window-procedure
- *
- * @param WindowHandle 윈도우 창에 대한 핸들입니다.
- * @param Message 윈도우 메시지 코드입니다.
- * @param WParam 윈도우 메시지의 추가 데이터입니다.
- * @param LParam 윈도우 메시지의 추가 데이터입니다.
- *
- * @return 윈도우에 반환할 값입니다.
- */
-LRESULT CALLBACK WindowProc(HWND WindowHandle, uint32_t Message, WPARAM WParam, LPARAM LParam)
-{
-	switch (Message)
-	{
-	case WM_CLOSE:
-		DestroyWindow(WindowHandle);
-		break;
-
-	case WM_DESTROY:
-		PostQuitMessage(0);
-		break;
-
-	default:
-		return DefWindowProc(WindowHandle, Message, WParam, LParam);
-	}
-
-	return 0;
-}
 
 
 /**
@@ -58,7 +25,7 @@ int32 WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR CmdLi
 
 	WC.cbSize         = sizeof(WNDCLASSEXW);
 	WC.style          = CS_HREDRAW | CS_VREDRAW;
-	WC.lpfnWndProc    = WindowProc;
+	WC.lpfnWndProc    = GetWindowMessageHandlerPtr();
 	WC.cbClsExtra     = 0;
 	WC.cbWndExtra     = 0;
 	WC.hInstance      = GetModuleHandle(nullptr);
@@ -84,19 +51,15 @@ int32 WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR CmdLi
 
 	ShowWindow(WindowHandle, SW_SHOW);
 
-	MSG Message;
+	bool bIsDone = false;
+	BindWindowCallbackEvent(EWindowEvent::CLOSE, [&]() {
+		bIsDone = true;
+	});
 
-	while (TRUE)
+	while (!bIsDone)
 	{
-		while (PeekMessage(&Message, NULL, 0, 0, PM_REMOVE))
-		{
-			TranslateMessage(&Message);
-			DispatchMessage(&Message);
-		}
-
-		if (Message.message == WM_QUIT)
-			break;
+		PollWindowEvent();
 	}
 
-	return static_cast<int32>(Message.wParam);
+	return 0;
 }
