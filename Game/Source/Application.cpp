107b5c651b6720080b7b9e650a123372057837df
DESKTOP-N6FOOG4\choij <choijione02@gmail.com>
2023-02-19 11:11:42 +0900

    디버그 모드에서 메모리 누수를 감지하는 코드 추가

--- Game/Source/Application.cpp
+++ Game/Source/Application.cpp
@@ -1,13 +1,19 @@
+#define _CRTDBG_MAP_ALLOC
+#include <crtdbg.h>
+
 int main(int argc, char* argv[])
 {
-	__try
-	{
+#if defined(DEBUG) || defined(_DEBUG)
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+#endif
 
-	}
-	__except (0)
-	{
 
-	}
+
+
+#if defined(DEBUG) || defined(_DEBUG)
+	_CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_DEBUG);
+	_CrtDumpMemoryLeaks();
+#endif
 
 	return 0;
 }
