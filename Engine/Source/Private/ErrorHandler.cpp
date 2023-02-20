e47c29e0ef4a2fa203c40fd5f3081b5d9b944be9
DESKTOP-N6FOOG4\choij <choijione02@gmail.com>
2023-02-20 09:20:41 +0900

    에러 처리 및 크래시 덤프를 생성하는 ErrorHandler class 선언 및 구현

---
+++ Engine/Source/Private/ErrorHandler.cpp
@@ -0,0 +1,80 @@
+#include "ErrorHandler.h"
+#include "Text.hpp"
+
+std::string ErrorHandler::CrashDumpPath_;
+std::string ErrorHandler::LastErrorMessage_;
+std::string ErrorHandler::ErrorFileName_;
+int32_t ErrorHandler::ErrorLine_;
+
+LONG ErrorHandler::GenerateCrashDump(EXCEPTION_POINTERS* ExceptionInfo)
+{
+	SYSTEMTIME CurrentSystemTime;
+	GetLocalTime(&CurrentSystemTime);
+
+	std::string CrashDumpFile = Format(
+		"%s%d-%d-%d-%d-%d-%d.dmp",
+		CrashDumpPath_.c_str(),
+		CurrentSystemTime.wYear,
+		CurrentSystemTime.wMonth,
+		CurrentSystemTime.wDay,
+		CurrentSystemTime.wHour,
+		CurrentSystemTime.wMinute,
+		CurrentSystemTime.wSecond
+	);
+
+	HANDLE  FileHandle = CreateFileA(
+		CrashDumpFile.c_str(),
+		GENERIC_WRITE,
+		FILE_SHARE_WRITE,
+		NULL,
+		CREATE_ALWAYS,
+		FILE_ATTRIBUTE_NORMAL,
+		NULL
+	);
+
+	if (FileHandle != INVALID_HANDLE_VALUE)
+	{
+		BOOL Successed = TRUE;
+
+		_MINIDUMP_EXCEPTION_INFORMATION Exception;
+		Exception.ThreadId = GetCurrentThreadId();
+		Exception.ExceptionPointers = ExceptionInfo;
+		Exception.ClientPointers = FALSE;
+
+		Successed = MiniDumpWriteDump(GetCurrentProcess(), GetCurrentProcessId(), FileHandle, MiniDumpNormal, &Exception, NULL, NULL);
+		if (!Successed)
+		{
+#if defined(DEBUG) || defined(_DEBUG)
+			OutputDebugStringA("failed to generate crash dump file");
+#endif
+		}
+
+		Successed = CloseHandle(FileHandle);
+		if (!Successed)
+		{
+#if defined(DEBUG) || defined(_DEBUG)
+			OutputDebugStringA("failed to close crash dump file");
+#endif
+		}
+	}
+	else
+	{
+#if defined(DEBUG) || defined(_DEBUG)
+		OutputDebugStringA("failed to generate crash dump file");
+#endif
+	}
+
+	return EXCEPTION_EXECUTE_HANDLER;
+}
+
+void ErrorHandler::ShowErrorMessageBox()
+{
+	std::string ShowErrorMessage = Format(
+		"FILE: %s\nLINE: %d\nMESSAGE: %s",
+		ErrorFileName_.c_str(),
+		std::to_string(ErrorLine_).c_str(),
+		LastErrorMessage_.c_str()
+	);
+
+	int32_t Successed = MessageBoxA(nullptr, ShowErrorMessage.c_str(), "Error Message", MB_OK);
+}
