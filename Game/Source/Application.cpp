a0b0ca43da512c3bdb71a8362df3b8075a3ee9c6
DESKTOP-N6FOOG4\choij <choijione02@gmail.com>
2023-02-20 09:42:24 +0900

    애플리케이션을 실행하는 함수 및 main 함수에 주석 추가

--- Game/Source/Application.cpp
+++ Game/Source/Application.cpp
@@ -1,12 +1,41 @@
-#define _CRTDBG_MAP_ALLOC
-#include <iostream>
-#include <crtdbg.h>
-
 #include "ErrorHandler.h"
+#include "Window.h"
 #include "WindowEventHandler.h"
 
 
-int main(int argc, char* argv[])
+/**
+ * @brief 애플케이션을 실행합니다.
+ * 
+ * @param Argc 명령행 인자의 수입니다.
+ * @param Argv 명령행 인자입니다.
+ * 
+ * @throws 애플리케이션 실행 중 에러 발생 시 예외를 던집니다.
+ */
+void RunApplication(int32_t Argc, char** Argv)
+{
+	std::unique_ptr<Window> MainWindow = std::make_unique<Window>(L"Tetris2D", 200, 200, 1000, 800, false);
+
+	bool bIsDone = false;
+	WindowEventHandler::BindCallbackEvent(WindowEventHandler::EEventType::CLOSE, [&]() {
+		bIsDone = true;
+	});
+
+	while (!bIsDone)
+	{
+		WindowEventHandler::PollEventQueue();
+	}
+}
+
+
+/**
+ * @brief 애플리케이션의 진입점입니다.
+ * 
+ * @param Argc 명령행 인자의 수입니다.
+ * @param Argv 명령행 인자입니다.
+ * 
+ * @return 다른 프로그램에 전달할 수 있는 상태 코드를 반환합니다.
+ */
+int main(int32_t Argc, char** Argv)
 {
 #if defined(DEBUG) || defined(_DEBUG)
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
@@ -14,17 +43,12 @@ int main(int argc, char* argv[])
 
 	__try
 	{
-
+		RunApplication(Argc, Argv);
 	}
 	__except (ErrorHandler::GenerateCrashDump(GetExceptionInformation()))
 	{
 		ErrorHandler::ShowErrorMessageBox();
 	}
-
-#if defined(DEBUG) || defined(_DEBUG)
-	_CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_DEBUG);
-	_CrtDumpMemoryLeaks();
-#endif
-
+	
 	return 0;
 }
