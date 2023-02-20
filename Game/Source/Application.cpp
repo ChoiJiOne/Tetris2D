633d4d0e540dc17693bc1fa74dc84235750dd791
DESKTOP-N6FOOG4\choij <choijione02@gmail.com>
2023-02-20 09:21:19 +0900

    예외를 감지하고 크래시 덤프를 생성하는 코드 추가

--- Game/Source/Application.cpp
+++ Game/Source/Application.cpp
@@ -2,12 +2,24 @@
 #include <iostream>
 #include <crtdbg.h>
 
+#include "ErrorHandler.h"
+#include "WindowEventHandler.h"
+
+
 int main(int argc, char* argv[])
 {
 #if defined(DEBUG) || defined(_DEBUG)
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 #endif
 
+	__try
+	{
+
+	}
+	__except (ErrorHandler::GenerateCrashDump(GetExceptionInformation()))
+	{
+		ErrorHandler::ShowErrorMessageBox();
+	}
 
 #if defined(DEBUG) || defined(_DEBUG)
 	_CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_DEBUG);
