691b7b8a8dfc871508f96dda6f679db9de9ad315
DESKTOP-N6FOOG4\choij <choijione02@gmail.com>
2023-02-21 19:00:52 +0900

    메인 함수 내에 예외 처리 부분 추가

--- Game/Source/Application.cpp
+++ Game/Source/Application.cpp
@@ -1,5 +1,6 @@
 #include "GameEngine.h"
 #include "Button.h"
+#include "ErrorHandler.h"
 #include "GraphicsManager.h"
 #include "Label.h"
 #include "InputManager.h"
@@ -180,20 +181,29 @@ private:
 
 
 /**
- * @brief Windows 애플리케이션 진입점입니다.
- * 
- * @see https://learn.microsoft.com/ko-kr/windows/win32/learnwin32/winmain--the-application-entry-point
- * 
- * @param hInstance 인스턴스에 대한 핸들입니다.
- * @param hPrevInstance 아무 의미 없는 파라미터입니다.
- * @param CmdLine 명령줄 인수입니다.
- * @param CmdShow 기본 애플리케이션 창을 최소화할지, 최대화할지 또는 정상적으로 표시할지를 나타내는 플래그입니다.
- * 
+ * @brief 애플리케이션의 진입점입니다.
+ *
+ * @param Argc 명령행 인자의 수입니다.
+ * @param Argv 명령행 인자입니다.
+ *
  * @return 다른 프로그램에 전달할 수 있는 상태 코드를 반환합니다.
  */
-int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR CmdLine, int32_t CmdShow)
+int32_t main(int32_t Argc, char** Argv)
 {
-	auto Game = std::make_unique<Tetris>();
-	Game->Initialize();
-	return Game->Run();
+#if defined(DEBUG) || defined(_DEBUG)
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+#endif
+
+	__try
+	{
+		auto Game = std::make_unique<Tetris>();
+		Game->Initialize();
+		Game->Run();
+	}
+	__except (ErrorHandler::GenerateCrashDump(GetExceptionInformation()))
+	{
+		ErrorHandler::ShowErrorMessageBox();
+	}
+
+	return 0;
 }
